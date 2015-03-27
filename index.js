0a5fd75f9155062792bb00bde59a9606bce86c07
lowsky <skylab71@gmail.com>
2015-03-27 17:44:09 +0100

    Express app serving the project root folder.

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ var app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('*', function(req, res) {
-    res.send('Hello express world').end();
-});
+app.use(express.static(__dirname, '/'));
 
 app.listen(app.get('port'), function() {
-    console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+    console.log( 'Express app started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
 });
