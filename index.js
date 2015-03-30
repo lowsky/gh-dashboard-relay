febf982f2e3021814cfbdf91ff236e0c432ac88f
lowsky <skylab71@gmail.com>
2015-03-31 00:05:01 +0200

    formatting

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var app = express();
 
@@ -5,6 +7,6 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname, '/'));
 
-app.listen(app.get('port'), function() {
-    console.log( 'Express app started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.' );
+app.listen(app.get('port'), function () {
+    console.log('Express app started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
