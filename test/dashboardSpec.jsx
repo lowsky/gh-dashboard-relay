b08550307fd1adae01eca5bcff4eb29168e91c2e
lowsky <skylab71@gmail.com>
2015-12-17 22:43:46 +0100

    use more ES6 features is test.

--- test/dashboardSpec.jsx
+++ test/dashboardSpec.jsx
@@ -1,5 +1,3 @@
-'use strict';
-
 import jsdom from 'jsdom';
 
 import React, { TestUtils } from 'react/addons';
@@ -10,10 +8,10 @@ import Dashboard from '../src/dashboard';
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = document.defaultView;
 
-describe('Dashboard branch component', function() {
+describe('Dashboard branch component', () => {
 
-    describe('rendering', function() {
-        var tableComp,
+    describe('rendering', () => {
+        let tableComp,
             dashComp;
 
         beforeEach(function () {
@@ -21,9 +19,8 @@ describe('Dashboard branch component', function() {
             dashComp = TestUtils.findRenderedComponentWithType(tableComp, Dashboard);
         });
 
-        it('should have class warning', function() {
-
+        it('should have class warning', () => {
             expect(React.findDOMNode(dashComp).className).to.equal('warning');
-        })
+        });
     });
 });
