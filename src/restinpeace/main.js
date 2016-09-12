938db8e1a1fc3fc5c6001e7b46939740431a7fa7
lowsky <lowsky@users.noreply.github.com>
2016-09-12 15:18:14 +0200

    move sources for old, restish way into sub folder

--- src/restinpeace/main.js
+++ src/restinpeace/main.js
@@ -2,7 +2,7 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
 import ReactDOM from 'react-dom';
 
-import BranchesTable from './BranchesTable'; // eslint-disable-line no-unused-vars
+import BranchesTable from '../components/BranchesTable'; // eslint-disable-line no-unused-vars
 
 const repo = 'lowsky/dashboard';
 
--- test/components/BranchInfoRowSpec.jsx
+++ test/components/BranchInfoRowSpec.jsx
@@ -2,7 +2,7 @@ import React from 'react'; // eslint-disable-line no-unused-vars
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
 
-import BranchInfoRow from '../src/BranchInfoRow'; // eslint-disable-line no-unused-vars
+import BranchInfoRow from '../../src/components/BranchInfoRow';
 
 describe('BranchInfoRow component', () => {
     const BRANCH_NAME = 'someBranchName';
