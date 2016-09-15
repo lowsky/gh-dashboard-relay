133cf016ce59de0badde7d522769f577592c192c
lowsky <lowsky@users.noreply.github.com>
2016-09-15 18:08:02 +0200

    switch to promised based whatwg-fetch, and add loading simple branch info

--- src/restinpeace/main.js
+++ src/restinpeace/main.js
@@ -5,28 +5,59 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import BranchesTable from '../components/BranchesTable';
+import User from '../components/User';
 
 const repo = 'lowsky/dashboard';
 
 let content = document.getElementById('content');
 
-let renderOrUpdateBranches = (branches) => {
-    const repoData = {
-        branches
-    };
+const githubData = {
+    repo: {
+        branches: []
+    },
+    user: {
+        avatar_url: '',
+        login: 'lowsky'
+    }
+};
+
+let renderOrUpdateBranches = () => {
 
     ReactDOM.render((
         <div className="panel-default">
                 <div className="panel-body">
-                    <BranchesTable repo={repoData} />
+                    <User user={githubData.user} />
+                    <BranchesTable repo={githubData.repo} />
                 </div>
         </div>),
         content);
 };
 
 // init
-renderOrUpdateBranches([]);
+renderOrUpdateBranches();
 
 // fetch per github REST api
-const { requestAndUpdateBranches } = fetchGithubApi;
-requestAndUpdateBranches(repo, renderOrUpdateBranches);
+const { fetchRepoBranches, fetchUser } = fetchGithubApi;
+
+fetchUser('lowsky')
+    .then(user => {
+        githubData.user = user;
+        console.log('fetched user data:', user);
+        renderOrUpdateBranches();
+    })
+    .catch(function (ex) {
+        console.log('fetching user info failed', ex);
+        alert(`Error, while trying to retrieve data for user.`); // eslint-disable-line quotes
+    });
+
+fetchRepoBranches(repo)
+    .then(branches => {
+        githubData.repo = {
+            branches
+        };
+        renderOrUpdateBranches();
+    })
+    .catch(function (ex) {
+        console.log('fetching branches info failed', ex);
+        alert(`Error, while trying to retrieve data for branches of repo.`); // eslint-disable-line quotes
+    });
