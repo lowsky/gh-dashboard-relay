14e72354142a28fb17e600739f8f82cfb925ee50
Marco Schaefer <msfr-develop@posteo.me>
2017-03-10 16:24:28 +0100

    change UserRepo to a MDL card

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
 const Repo = props => {
-    const { repo = {} } = props;
-    const { owner = {}, name } = repo;
+    const {repo = {}} = props;
+    const {owner = {}, name} = repo;
 
-    return (<div> {owner.login} / {name} </div>);
+    return (
+        <div className="mdl-card mdl-shadow--2dp">
+            <div className="mdl-card__title">
+                <h2 className="mdl-card__title-text">Repository</h2>
+            </div>
+            <div className="mdl-card__supporting-text">
+                {owner.login} / {name}
+            </div>
+            <div className="mdl-card__menu">
+                <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+                    <i className="material-icons">link</i>
+                </button>
+            </div>
+        </div>
+    );
 };
 
 export default Repo;
