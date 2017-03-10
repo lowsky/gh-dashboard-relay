0e6d99b14db9289fed2252ba8b2310ea8572b22f
Marco Schaefer <msfr-develop@posteo.me>
2017-03-10 15:39:20 +0100

    add mdl to RestfulDemo

--- src/container/UserRepo.js
+++ src/container/UserRepo.js
@@ -10,36 +10,32 @@ let UserRepo = (props) => {
     const Repo = Ui.createRepo(props);
     const BranchesTable = Ui.createBranchTable(props);
 
+    componentDidMount: () => window.componentHandler.upgradeElement(this.root);
+
+    componentWillUnmount: () => window.componentHandler.downgradeElements(this.root);
+
     return (
-        <div>
-            <div className="panel-default">
-                <div className="panel-heading">
-                    <h1 className="panel-title"> Repository: </h1>
+            <div className="demo-card-wide mdl-card mdl-shadow--2dp">
+                <div className="mdl-card__title">
+                    <h2 className="mdl-card__title-text">Welcome</h2>
                 </div>
-                <div className="panel-body">
-                    {
-                        <Repo repo={repo}/>
-                    }
+                <div className="mdl-card__supporting-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Mauris sagittis pellentesque lacus eleifend lacinia...
                 </div>
-            </div>
-            <div className="panel-default">
-                <div className="panel-heading">
-                    <h1 className="panel-title">Owner:</h1>
+                <div className="mdl-card__actions mdl-card--border">
+                    <a className="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+                    Get Started
+                    </a>
                 </div>
-                <div className="panel-body">
-                    {
-                        <User user={user}/>
-                    }
+                <div className="mdl-card__menu">
+                    <button className="mdl-button mdl-button--icon mdl-js-button mdl-js-ripple-effect">
+                        <i className="material-icons">share</i>
+                    </button>
                 </div>
             </div>
-            <div className="panel-default">
-                <div className="panel-body">
-                    {
-                        <BranchesTable repo={repo}/>
-                    }
-                </div>
-            </div>
-        </div>);
+    );
+
 };
 
 export default UserRepo;
