fc424264062958f737ea9b188803ff595e6df3af
lowsky <lowsky@users.noreply.github.com>
2017-03-15 23:04:35 +0100

    enrich data for RESTful view with last commit statuses

--- src/restinpeace/restful.js
+++ src/restinpeace/restful.js
@@ -81,6 +81,48 @@ let enrich = (branch) => {
         'sha': 'bfae86ac1dc8d3b0ae594e2c3a23311dd503d15e',
         'message': 'Merge pull request #287 from lowsky/greenkeeper/jsdom-pin-9.11.0\n\nchore: pin jsdom to 9.11.0',
         'status': [
+            {
+                'state': 'success',
+                'description': 'Your tests passed on CircleCI!',
+                'target_url': 'https://circleci.com/gh/lowsky/dashboard/763?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link',
+                'context': 'ci/circleci',
+                'updated_at': '2017-03-14T18:59:30Z'
+            },
+            {
+                'state': 'success',
+                'description': 'No failing dependencies.',
+                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/bfae86ac1dc8d3b0ae594e2c3a23311dd503d15e/dependencies/npm?status=passing',
+                'context': 'bitHound - Dependencies',
+                'updated_at': '2017-03-14T18:55:43Z'
+            },
+            {
+                'state': 'success',
+                'description': 'No failing files.',
+                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/bfae86ac1dc8d3b0ae594e2c3a23311dd503d15e/files?status=passing',
+                'context': 'bitHound - Code',
+                'updated_at': '2017-03-14T18:55:43Z'
+            },
+            {
+                'state': 'pending',
+                'description': null,
+                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/master',
+                'context': 'bitHound - Dependencies',
+                'updated_at': '2017-03-14T18:54:57Z'
+            },
+            {
+                'state': 'pending',
+                'description': null,
+                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/master',
+                'context': 'bitHound - Code',
+                'updated_at': '2017-03-14T18:54:57Z'
+            },
+            {
+                'state': 'success',
+                'description': 'GitHub Pages successfully built your site.',
+                'target_url': null,
+                'context': 'github/pages',
+                'updated_at': '2017-03-14T18:54:50Z'
+            }
         ],
         'author': {
             'login': 'lowsky',
