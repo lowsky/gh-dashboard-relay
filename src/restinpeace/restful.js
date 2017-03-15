4e0e3b840526f4d4fb31274bf2e54d4cd8e9d4ca
lowsky <lowsky@users.noreply.github.com>
2017-03-15 23:16:52 +0100

    refactor, improve error messages

--- src/restinpeace/restful.js
+++ src/restinpeace/restful.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-console */
 
-import fetchGithubApi from './fetchGithubApi';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import UserRepo from '../container/UserRepo';
+import fetchGithubApi from './fetchGithubApi';
+
+const lastCommitMock = require('./lastCommitMock.json');
 
 const repo = 'lowsky/dashboard';
 
@@ -51,9 +53,9 @@ fetchUser('lowsky')
         console.log('fetched user data:', user);
         renderOrUpdateBranches();
     })
-    .catch(function (ex) {
+    .catch(ex => {
         console.log('fetching user info failed', ex);
-        alert(`Error, while trying to retrieve data for user.`); // eslint-disable-line quotes
+        alert(`Error, while loading user info for user ($user) from github`); // eslint-disable-line quotes
     });
 
 fetchRepoBranches(repo)
@@ -66,68 +68,12 @@ fetchRepoBranches(repo)
     })
     .then(branches => {
         githubData.repo = {
-            branches: branches.map(branch => enrich(branch))
+            branches: branches.map(branch => { branch.lastCommit = lastCommitMock; return branch; })
         };
         renderOrUpdateBranches();
     })
-    .catch((ex) => {
+    .catch(ex => {
         console.log('fetching branches info failed', ex);
-        // alert(`Error, while trying to retrieve data for branches of repo.`); // eslint-disable-line quotes
+        alert(`Error, while loading branches info for repo ($repo) from github`); // eslint-disable-line quotes
     });
 
-let enrich = (branch) => {
-    branch.lastCommit = {
-        date: '',
-        'sha': 'bfae86ac1dc8d3b0ae594e2c3a23311dd503d15e',
-        'message': 'Merge pull request #287 from lowsky/greenkeeper/jsdom-pin-9.11.0\n\nchore: pin jsdom to 9.11.0',
-        'status': [
-            {
-                'state': 'success',
-                'description': 'Your tests passed on CircleCI!',
-                'target_url': 'https://circleci.com/gh/lowsky/dashboard/763?utm_campaign=vcs-integration-link&utm_medium=referral&utm_source=github-build-link',
-                'context': 'ci/circleci',
-                'updated_at': '2017-03-14T18:59:30Z'
-            },
-            {
-                'state': 'success',
-                'description': 'No failing dependencies.',
-                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/bfae86ac1dc8d3b0ae594e2c3a23311dd503d15e/dependencies/npm?status=passing',
-                'context': 'bitHound - Dependencies',
-                'updated_at': '2017-03-14T18:55:43Z'
-            },
-            {
-                'state': 'success',
-                'description': 'No failing files.',
-                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/bfae86ac1dc8d3b0ae594e2c3a23311dd503d15e/files?status=passing',
-                'context': 'bitHound - Code',
-                'updated_at': '2017-03-14T18:55:43Z'
-            },
-            {
-                'state': 'pending',
-                'description': null,
-                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/master',
-                'context': 'bitHound - Dependencies',
-                'updated_at': '2017-03-14T18:54:57Z'
-            },
-            {
-                'state': 'pending',
-                'description': null,
-                'target_url': 'https://www.bithound.io/github/lowsky/dashboard/master',
-                'context': 'bitHound - Code',
-                'updated_at': '2017-03-14T18:54:57Z'
-            },
-            {
-                'state': 'success',
-                'description': 'GitHub Pages successfully built your site.',
-                'target_url': null,
-                'context': 'github/pages',
-                'updated_at': '2017-03-14T18:54:50Z'
-            }
-        ],
-        'author': {
-            'login': 'lowsky',
-            'avatar_url': 'https://avatars2.githubusercontent.com/u/217931?v=3'
-        }
-    };
-    return branch;
-};
