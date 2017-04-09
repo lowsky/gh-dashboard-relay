511ab90a01ebeb3fc7ffb6920b3137fa5c67d44e
lowsky <lowsky@users.noreply.github.com>
2017-04-09 17:58:55 +0200

    material-ui: have repo’s owner logo completely contained in User card

--- src/components/User.js
+++ src/components/User.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 const card = {
     width: '320px',
-    height: '320px',
-    float: 'right'
 };
 
 const titleBg = (url) => {
     return {
         color: '#fff',
-        background: `url(${url}) no-repeat center cover`,
+        backgroundImage: `url("${url}")`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat'
     };
 };
 
@@ -26,15 +26,8 @@ const User = props => {
         <div className="mdl-card mdl-shadow--2dp" style={{...card, ...titleBg(avatar_url)}}>
             <div className="mdl-card__title mdl-card--expand"></div>
             <div className="mdl-card__actions">
-                <span className="demo-card-image__filename"><b>{ login }</b> @{ company }</span>
+                <span className="demo-card-image__filename"><b>{ login }</b> { company }</span>
             </div>
-            {/*<div className="mdl-card__title mdl-card--expand" style={titleBg(avatar_url)}>*/}
-                {/*<h2 className="mdl-card__title-text">Owner</h2>*/}
-            {/*</div>*/}
-            {/*<div className="mdl-card__supporting-text">*/}
-                {/*{ login }*/}
-                {/*<p>@{ company }</p>*/}
-            {/*</div>*/}
         </div>
     );
 };
