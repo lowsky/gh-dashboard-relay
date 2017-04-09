b10be35a86b7741353b3a04fa20d7468a57f2888
lowsky <lowsky@users.noreply.github.com>
2017-04-09 17:34:44 +0200

    remove typo/extra chars

--- src/components/Repo.js
+++ src/components/Repo.js
@@ -12,7 +12,7 @@ const Repo = props => {
 
     return (
         <div className="mdl-card mdl-shadow--2dp" style={card}>
-            <div className="mdl-card__title">                     ¡¡
+            <div className="mdl-card__title">
                 <h2 className="mdl-card__title-text">Repository</h2>
             </div>
             <div className="mdl-card__supporting-text">
