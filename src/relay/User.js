ccee11059d0145b8a8c16fd75ca5d064219c374e
lowsky <lowsky@users.noreply.github.com>
2017-07-22 12:28:34 +0200

    bad formatting...

--- src/relay/User.js
+++ src/relay/User.js
@@ -2,14 +2,16 @@ import Relay from 'react-relay';
 
 import User from '../components/User';
 
-export default Relay.createContainer(User, {
+// bad formatting:
+export
+default
+Relay.createContainer(User, {
     fragments: {
         user: () => Relay.QL`
-            fragment on GithubUser {
-                login
-                company
-                avatar_url
-            }
+fragment on GithubUser {
+    login
+avatar_url
+}
         `,
     },
     name: 'User',
