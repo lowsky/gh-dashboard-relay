0333c110094723d622985e498203e412329e7425
lowsky <lowsky@users.noreply.github.com>
2017-10-22 12:06:33 +0200

    add padding around Readme content

--- src/index/indexPage.js
+++ src/index/indexPage.js
@@ -3,6 +3,10 @@ import React from 'react';
 /* just include the README content on the start page */
 const __html = require('html-loader!../../README.html');
 
-const indexPageContent = () => <div dangerouslySetInnerHTML={{ __html }} />;
+const indexPageContent = () => (
+    <div style={{ padding: '3em' }}>
+        <div dangerouslySetInnerHTML={{ __html }} />
+    </div>
+);
 
 export default indexPageContent;
