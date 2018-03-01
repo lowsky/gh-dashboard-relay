509e01c6b0a3d4b13cf179337b853958a1a3d135
lowsky <lowsky@users.noreply.github.com>
2018-03-01 23:03:04 +0100

    DB-511 add tests

--- src/container/__tests__/BranchInfoRow-test.js
+++ src/container/__tests__/BranchInfoRow-test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { expect } from 'chai';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-
 Enzyme.configure({ adapter: new Adapter() });
-import BranchInfoRow from '../../src/container/BranchInfoRow';
+
+import BranchInfoRow from '../../container/BranchInfoRow';
 
 describe('Dashboard branch component', () => {
     const BRANCH_NAME = 'someBranchName';
@@ -14,19 +13,19 @@ describe('Dashboard branch component', () => {
     const dashComp = shallow(<BranchInfoRow branch={branch} />);
 
     it('should render to a <tr> without className per default', () => {
-        expect(dashComp.type()).to.equal('tr');
+        expect(dashComp.type()).toBe('tr');
     });
 
     it('should have 2 columns', () => {
         const columns = dashComp.find('td');
 
-        expect(columns).to.have.length(2);
+        expect(columns).toHaveLength(2);
     });
 
     it('first column should contain a link with text containing the branchName', () => {
         const firstColumn = dashComp.childAt(0);
         const aLink = firstColumn.find('a');
 
-        expect(aLink.props().children).to.equal(BRANCH_NAME);
+        expect(aLink.props().children).toBe(BRANCH_NAME);
     });
 });
