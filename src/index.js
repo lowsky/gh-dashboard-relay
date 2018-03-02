e7dcd4e22b5bb88c0f3f1e4de036fc4db30f9272
lowsky <217931+lowsky@users.noreply.github.com>
2018-03-02 16:06:53 +0100

    DB-511 remove obsolete deps (#515)
    * remove chai, mocha nd istanbul
    
    * cleanup babel config
    
    * remove react-hot-reloader
    
    * fix/reorder package deps
    
    * remove obsolete deps (and move from stage-0 to stage-3)

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import routes from './routes';
-
-import { AppContainer } from 'react-hot-loader';
+import Routes from './routes';
 
 const rootElement = document.getElementById('root');
 
-const render = Component => {
-    ReactDOM.render(
-        <AppContainer>
-            <Component />
-        </AppContainer>,
-        rootElement
-    );
-};
-render(routes);
-
-// Webpack Hot Module Replacement API
-if (module.hot) {
-    module.hot.accept('./routes', () => {
-        render(routes);
-    });
-}
+ReactDOM.render(<Routes />, rootElement);
