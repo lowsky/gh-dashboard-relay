57c9fc2310c8e4aebd633a69aec69591c47197fc
lowsky <lowsky@users.noreply.github.com>
2018-11-10 11:08:21 +0100

    use React.lazy for lazy loading .

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,9 @@
-import React, {lazy} from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import IndexPageMain from './index/indexPage';
 import { NavBar } from './components/NavBar';
+import { Spinner } from './components/Spinner';
 
 const RelayMain = lazy(() => import('./relay/main'));
 const RestfulMain = lazy(() => import('./restinpeace/restful'));
@@ -18,14 +19,16 @@ const MainPage = () => (
             </header>
 
             <main>
-                <Switch>
-                    <Redirect exact from="/" to="/home" />
-                    <Route exact path="/home" component={IndexPageMain} />
-                    <Route path="/relay/:repo" component={RelayMain} />
-                    <Route path="/restful/:userName/:repoName" component={RestfulMain} />
-                    <Redirect from="/relay" to="/relay/lowsky/dashboard" />
-                    <Redirect from="/restful" to="/restful/lowsky/dashboard" />
-                </Switch>
+                <Suspense fallback={<Spinner />}>
+                    <Switch>
+                        <Redirect exact from="/" to="/home" />
+                        <Route exact path="/home" component={IndexPageMain} />
+                        <Route path="/relay/:repo" component={RelayMain} />
+                        <Route path="/restful/:userName/:repoName" component={RestfulMain} />
+                        <Redirect from="/relay" to="/relay/lowsky/dashboard" />
+                        <Redirect from="/restful" to="/restful/lowsky/dashboard" />
+                    </Switch>
+                </Suspense>
             </main>
         </div>
     </Router>
