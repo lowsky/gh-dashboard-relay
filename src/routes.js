1059eeafc9345fde4a8a29f272cc32c167d86555
lowsky <217931+lowsky@users.noreply.github.com>
2018-11-10 12:07:01 +0100

    Split modules lazy loading (#705)
    * add React.lazy
    
    * reorder imports
    
    * use React.lazy for lazy loading .

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
 
 import IndexPageMain from './index/indexPage';
-import RelayMain from './relay/main';
-import RestfulMain from './restinpeace/restful';
 import { NavBar } from './components/NavBar';
+import { Spinner } from './components/Spinner';
+
+const RelayMain = lazy(() => import('./relay/main'));
+const RestfulMain = lazy(() => import('./restinpeace/restful'));
 
 const MainPage = () => (
     <Router>
@@ -17,14 +19,16 @@ const MainPage = () => (
             </header>
 
             <main>
-                <Switch>
-                    <Redirect exact from="/" to="/home" />
-                    <Route exact path="/home" component={IndexPageMain} />
-                    <Route path="/relay/:repo" component={RelayMain} />
-                    <Route path="/restful/:userName/:repoName" component={RestfulMain} />
-                    <Redirect from="/relay" to="/relay/lowsky/dashboard" />
-                    <Redirect from="/restful" to="/restful/lowsky/dashboard" />
-                </Switch>
+                <Suspense fallback={<Spinner />}>
+                    <Switch>
+                        <Redirect exact from="/" to="/home" />
+                        <Route exact path="/home" component={IndexPageMain} />
+                        <Route path="/relay/:repo" component={RelayMain} />
+                        <Route path="/restful/:userName/:repoName" component={RestfulMain} />
+                        <Redirect from="/relay" to="/relay/lowsky/dashboard" />
+                        <Redirect from="/restful" to="/restful/lowsky/dashboard" />
+                    </Switch>
+                </Suspense>
             </main>
         </div>
     </Router>
