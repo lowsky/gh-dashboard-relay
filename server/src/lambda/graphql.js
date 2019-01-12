244e6b02e3f4396505c58a730087c905a9e9c003
lowsky <lowsky@users.noreply.github.com>
2019-01-12 12:28:45 +0100

    cleanup graphql server part

--- server/src/lambda/graphql.js
+++ server/src/lambda/graphql.js
@@ -1,47 +1,11 @@
 const { GraphQLServerLambda } = require('graphql-yoga');
-let schema = require('../../localSchema');
-/*
-
+const schema = require('../../localSchema');
 
 const lambda = new GraphQLServerLambda({
     schema: schema.default,
-});
-*/
-
-import { execute } from 'graphql';
-
-const typeDefs = `
-  type Query {
-    hello(name: String): String
-  }
-`
-const resolvers = {
-    Query: {
-        //hello: (_, { name }) => `Hello ${name || 'world'}`,
-        hello: (_, { name }) => {
-
-            let x = execute(
-    schema.default,
-    'hello',
-    /*
-    rootValue,
-    contextValue,
-    variableValues,
-    operationName,
-    fieldResolver
-    */
-            );
-
-
-            return `Hello ${name || 'world'}`;
-        },
-    },
-}
-
-const lambda = new GraphQLServerLambda({
-    typeDefs,
-    schema: schema.default
-    //resolvers,
+    options: {
+        debug: false,
+    }
 });
 
 const handler = (event, lambdaContext, callback) => {
