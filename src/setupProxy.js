b3dcef499f3016e37851cf39b95e0315c961e3a8
lowsky <lowsky@users.noreply.github.com>
2019-01-13 18:11:40 +0100

    do proxying to local server at /graphql

---
+++ src/setupProxy.js
@@ -0,0 +1,9 @@
+const proxy = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        proxy('/graphql', {
+            target: 'http://localhost:4000/graphql',
+        })
+    );
+};
