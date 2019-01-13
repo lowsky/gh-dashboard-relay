15158772655f077583c1155c7260dbf730f2cc59
lowsky <lowsky@users.noreply.github.com>
2019-01-13 20:32:15 +0100

    switch to graphql-yoga

--- server/index.js
+++ server/index.js
@@ -1,61 +1,46 @@
 /* eslint-disable no-console */
+
+// loads github access token:
 require('dotenv/config');
+
 const express = require('express');
 const helmet = require('helmet');
 
-// allow requests in webpack-dev-server mode (on :8080)
-const cors = require('cors');
+const { GraphQLServer } = require('graphql-yoga');
 
-const corsOptions = {
-    origin: 'http://localhost:8080',
-    optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+let schema = require('./localSchema');
+
+const server = new GraphQLServer({
+    schema: schema.default,
+});
+const options = {
+    endpoint: '/graphql',
+    subscriptions: false,
+    playground: '/playground',
+    debug: true,
 };
 
-const app = express();
+server.start(options, ({ port }) => {
+    console.log(`Server is running on http://localhost:${port}`);
 
-app.use(helmet.hidePoweredBy());
-// disable IEx to open other files with same rights
-app.use(helmet.ieNoOpen());
-// Sets "X-Content-Type-Options: nosniff".
-app.use(helmet.noSniff());
+    const app = server.express;
 
-app.set('port', process.env.PORT || 3000);
+    app.use(helmet.hidePoweredBy());
+    // disable IEx to open other files with same rights
+    app.use(helmet.ieNoOpen());
+    // Sets "X-Content-Type-Options: nosniff".
+    app.use(helmet.noSniff());
 
-app.use(express.static('dist'));
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static('../build'));
+        console.log('Running in production mode, serving web client from ../build.');
+    } else {
+        app.use(express.static('../public'));
+        console.log('Running in dev mode, serving only ../public folder');
+    }
 
-// mutation : GraphQLHub.MutationsType,
-
-// rootValue: req.rootValue,
-// formatError: formatError,
-// pretty: req.query.pretty,
-
-let expressGraphQL = require('express-graphql');
-let schema = require('./localSchema');
-const path = require('path');
-
-app.use(cors(corsOptions));
-
-/* eslint-disable no-unused-vars,prettier/prettier */
-app.use('/graphql', expressGraphQL(req => ({
-    schema: schema.default,
-    graphiql: true,
-    pretty: true
-})));
-
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('../build'));
-    console.log('Running in production mode, serving web client from ../build.');
-}
-else {
-    app.use(express.static('../public'));
-    console.log('Running in dev mode, serving only ../public folder');
-}
-
-// doesn't work with webpack-dev-server :(
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, '../build', 'index.html'));
-});
-
-app.listen(app.get('port'), function () {
-    console.log(`Express app started on http://localhost:${app.get('port')} press Ctrl-C to terminate.`);
+    const path = require('path');
+    app.get('/*', function(req, res) {
+        res.sendFile(path.join(__dirname, '../build', 'index.html'));
+    });
 });
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ const proxy = require('http-proxy-middleware');
 module.exports = function(app) {
     app.use(
         proxy('/graphql', {
-            target: 'http://localhost:4000/graphql',
+            target: 'http://localhost:4000',
         })
     );
 };
