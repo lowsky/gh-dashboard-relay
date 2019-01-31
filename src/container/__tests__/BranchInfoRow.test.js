c2c1b62d23f0ac523568c5caf3233b91008ae512
lowsky <217931+lowsky@users.noreply.github.com>
2019-01-31 20:44:58 +0100

    add lint config from standard react-app (#767)
    in dev mode, react-app showed some extra eslint warnings, which should also be reported in lint run.
    
    We need to import default react eslint settings...

--- src/container/__tests__/BranchInfoRow.test.js
+++ src/container/__tests__/BranchInfoRow.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-Enzyme.configure({ adapter: new Adapter() });
-
 import BranchInfoRow from '../../container/BranchInfoRow';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Dashboard branch component', () => {
     const BRANCH_NAME = 'someBranchName';
