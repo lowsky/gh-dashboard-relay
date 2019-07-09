d4dbd68b3e6a72208fcb0c630fbd284c2a1dcaf8
lowsky <lowsky@users.noreply.github.com>
2019-07-09 12:49:51 +0200

    re-enable fix unit tests

--- src/container/__tests__/BranchInfoRow.test.js
+++ src/container/__tests__/BranchInfoRow.test.js
@@ -1,28 +1,26 @@
 import React from 'react';
-import Enzyme, { shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { shallow } from 'enzyme';
+
 import BranchInfoRow from '../../container/BranchInfoRow';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe('Dashboard branch component', () => {
+describe('BranchInfoRow component', () => {
     const BRANCH_NAME = 'someBranchName';
     const branch = {
         name: BRANCH_NAME,
     };
     const dashComp = shallow(<BranchInfoRow branch={branch} />);
 
-    xit('should render to a <tr> without className per default', () => {
+    it('should render to a <tr> without className per default', () => {
         expect(dashComp.type()).toBe('tr');
     });
 
-    xit('should have 2 columns', () => {
+    it('should have 2 columns', () => {
         const columns = dashComp.find('td');
 
         expect(columns).toHaveLength(2);
     });
 
-    xit('first column should contain a link with text containing the branchName', () => {
+    it('first column should contain a link with text containing the branchName', () => {
         const firstColumn = dashComp.childAt(0);
         const aLink = firstColumn.find('a');
 
