4eae53e2d1145c2238c1f2f21d546b3cd5afdbad
lowsky <217931+lowsky@users.noreply.github.com>
2019-08-14 01:33:01 +0200

    Visual regression testing (#845)
    * add visual regression tests using cypress
    
    * add visual regression testing into pipeline
    
    * caching cypress downloads
    
    * update circleci cache config
    
    * use cypress docker containers
    
    * add --ci to avoid storybook to open browser
    
    * adapt circleci setting for collecting artifacts
    
    * optimise pipeline: only run yarn install when needed
    
    * replace visual snapshots with docker built ones.

---
+++ cypress/integration/screening.js
@@ -0,0 +1,50 @@
+/* eslint-disable no-undef */
+
+// import {  } from 'cypress-match-screenshot';
+
+export const pages = [
+    'pages--main',
+    'userrepo--with-user-and-repo',
+    'user--with-avatar',
+    'branchestable--with-one-branch',
+];
+
+describe('VisualRegression', () => {
+    pages.map(path => {
+        const filename = path.replace(/^\//, '').replace(/\//gi, '_');
+
+        const flags = [];
+        if (Cypress.browser.isHeaded) flags.push('headed');
+        if (Cypress.browser.isHeadless) flags.push('headless');
+
+        it(path, () => {
+            cy.visit('/iframe.html?id=' + path)
+                .url()
+                .should('contain', path, LogAndWaitLong);
+            cy.wait(2500); // for loading all icons
+            // cy.get('body').type('A') /* shortcut for hiding addons view*/.type('S'); // shortcut for hiding stories view
+            cy.log('Filename: ' + filename);
+            doScreenshot(filename);
+
+            cy.matchScreenshot(flags.join('_'));
+
+            // snapshot name will be the test title
+
+            // NO
+            // cy.pause();
+        });
+    });
+});
+
+function doScreenshot(filename) {
+    cy.screenshot(filename, {
+        // capture: 'runner' | 'viewport' | 'fullPage'
+        capture: 'fullPage',
+        timeout: 50000,
+    });
+}
+
+let LogAndWaitLong = {
+    log: false,
+    timeout: 2000,
+};
