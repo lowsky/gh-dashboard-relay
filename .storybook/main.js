c2045fde5806221afad240a7bd9c64a98c842d8a
lowsky <lowsky@users.noreply.github.com>
2020-07-18 18:33:08 +0200

    activate docs and deps addons in storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,10 +2,11 @@ module.exports = {
     stories: ['../src/**/*.story.js'],
     addons: [
         '@storybook/preset-create-react-app',
-        'storybook-addon-jsx',
+        'storybook-addon-deps',
         'storybook-chromatic',
         '@storybook/addon-viewport',
         '@storybook/addon-actions',
+        '@storybook/addon-docs',
         '@storybook/addon-links',
     ],
 };
