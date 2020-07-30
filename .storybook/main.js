a85fbdd4a7a7a97e0650c19c526e7d1197e40579
lowsky <lowsky@users.noreply.github.com>
2020-07-30 10:17:59 +0200

    migrate, and remove obsolete parts

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,6 @@
 module.exports = {
     stories: ['../src/**/*.story.@(js|mdx)'],
     addons: [
-        '@storybook/preset-create-react-app',
         'storybook-addon-deps',
         '@storybook/addon-docs',
     ],
