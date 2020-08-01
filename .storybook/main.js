a2e0f8665486156672b1546af5a453878bbd2eab
lowsky <lowsky@users.noreply.github.com>
2020-08-01 16:28:42 +0200

    add @storybook/preset-create-react-app

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
     stories: ['../src/**/*.story.@(js|mdx)'],
     addons: [
+        '@storybook/preset-create-react-app',
         'storybook-addon-deps',
         '@storybook/addon-docs',
     ],
