66bdb4f162fc0747f31ff45ff648395dfa3bfc4a
lowsky <lowsky@users.noreply.github.com>
2020-11-22 19:52:49 +0100

    add bulma global styling for sb

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,7 @@
 import { DocsPage } from 'storybook-addon-deps/blocks';
 
+import 'bulma/css/bulma.css';
+
 export const parameters = {
     docs: { page: DocsPage },
     dependencies: { withStoriesOnly: true, hideEmpty: true },
