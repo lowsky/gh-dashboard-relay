9bb5c1b72d1bd12734f0ef3ca598077dc6e1544e
lowsky <lowsky@users.noreply.github.com>
2020-11-29 12:15:19 +0100

    switch to serverless functions

--- next.config.js
+++ next.config.js
@@ -1,2 +1,6 @@
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = {
+  ...withImages(),
+  // Target must be serverless (for use with netlify)
+  target: "serverless",
+};
