cdb61395e22334ab11bc81e3d32a933aac775351
lowsky <lowsky@users.noreply.github.com>
2021-03-27 21:03:56 +0100

    add missing error boundary component

---
+++ relay/ErrorBoundaryWithRetry.tsx
@@ -0,0 +1,24 @@
+/**
+ * from https://relay.dev/docs/guided-tour/rendering/error-states/
+ */
+import React from 'react';
+
+export default class ErrorBoundaryWithRetry extends React.Component<{ fallback: any }> {
+    state = { error: null };
+
+    static getDerivedStateFromError(error) {
+        return { error: error };
+    }
+
+    render() {
+        const { children, fallback } = this.props;
+        const { error } = this.state;
+        if (error) {
+            if (typeof fallback === 'function') {
+                return fallback({ error });
+            }
+            return fallback;
+        }
+        return children;
+    }
+}
