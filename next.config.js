41c668b60b239489b7eb77b37b13e7fc62487b8f
lowsky <lowsky@users.noreply.github.com>
2021-04-02 15:24:56 +0200

    enable webpack5 in next' build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@ const withImages = require('next-images')
 
 module.exports = {
   ...withImages(),
+  future: {
+    webpack5: true,
+  },
   // Target must be serverless (for use with netlify)
   target: "serverless",
 };
