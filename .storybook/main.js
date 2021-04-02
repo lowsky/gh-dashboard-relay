264417ce14322c28bf901696dd3cda46d1af8ee7
lowsky <lowsky@users.noreply.github.com>
2021-04-02 15:24:32 +0200

    storybook cosmetic cleanup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,10 @@
 module.exports = {
     stories: [
-        '../stories/*.story.@(js|jsx|ts|tsx|mdx)',
         '../components/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../lib/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../pages/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../queries/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../relay/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../restinpeace/*.story.@(js|jsx|ts|tsx|mdx)',
         '../container/*.story.@(js|jsx|ts|tsx|mdx)',
+        '../pages/*.story.@(js|jsx|ts|tsx|mdx)',
+        '../relay/*.story.@(js|jsx|ts|tsx|mdx)',
+        '../stories/*.story.@(js|jsx|ts|tsx|mdx)',
     ],
     addons: ['@storybook/addon-essentials'],
 };
