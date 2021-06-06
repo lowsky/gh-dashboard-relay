44046d50eb8bf15426eb73f4e7607675df080622
lowsky <lowsky@users.noreply.github.com>
2021-06-06 19:57:12 +0200

    add step to copy local-schema from one source

--- lib/localSchema.ts
+++ lib/localSchema.ts
@@ -1,74 +1,87 @@
-// Construct a schema, using GraphQL schema language
+
+    // Auto-generated, instead of using a webpack-loader...
+    // Just wrapping schema/schema.graphql
 export const typeDefs = `
-    # This file was generated based on ".graphqlconfig". Do not edit manually.
+# source: http://localhost:3000/graphql
+# timestamp: Sun Feb 25 2018 02:36:17 GMT+0100 (CET)
 
-    type Query {
-        github: GithubAPI
-        repo("Name of the repo" name: String!, "Username of the owner" ownerUsername: String!): GithubRepo
-        user("Username of the user" username: String!): GithubUser
-    }
+schema {
+    query: GraphQL_github_API
+}
 
-    union UserOrCommitAuthor = GithubCommitAuthor | GithubUser
-
-    "The Github API"
-    type GithubAPI {
-        repo("Name of the repo" name: String!, "Username of the owner" ownerUsername: String!): GithubRepo
-        user("Username of the user" username: String!): GithubUser
-    }
-
-    type GithubBranch {
-        ownerUsername: String
-        reponame: String
-
-        lastCommit: GithubCommit
+# The Github API
+type GithubAPI {
+    user(
+        # Username of the user
+        username: String!
+    ): GithubUser
+    repo(
+        # Name of the repo
         name: String!
-    }
 
-    type GithubCommit {
-        author: UserOrCommitAuthor
-        date: String
-        message: String
-        sha: String
-        url: String
-        status: [GithubStatus]
-        # tree: GithubTree
-    }
+        # Username of the owner
+        ownerUsername: String!
+    ): GithubRepo
+}
 
-    "Commit author that is not associated with a Github acount"
-    type GithubCommitAuthor {
-        email: String
-        name: String
-    }
+type GithubBranch {
+    ownerUsername: String
+    reponame: String
+    name: String!
+    lastCommit: GithubCommit
+}
 
-    type GithubRepo {
-        branches(limit: Int): [GithubBranch]
-        commits(limit: Int): [GithubCommit]
-        id: String
-        name: String
-        owner: GithubUser
-    }
+type GithubCommit {
+    sha: String
+    author: UserOrCommitAuthor
+    message: String
+    date: String
+    status: [GithubStatus]
+    associatedPullRequests: [PullRequest]
+}
 
-    "Status of a commit"
-    type GithubStatus {
-        context: String # e.g. UI Tests
-        description: String
-        state: String # e.g. success
-        target_url: String
-        updated_at: String
-        avatar_url: String
-        url: String
-        # creator: User...
-        # "id": 10062999186,
-        # "node_id": "MDEzOlN0YXR1c0NvbnRleHQxMDA2Mjk5OTE4Ng==",
-        # "created_at": "2020-06-29T08:37:19Z",
-        # "updated_at": "2020-06-29T08:37:19Z",
-    }
+# Commit author that is not associated with a Github acount
+type GithubCommitAuthor {
+    email: String
+    name: String
+}
 
-    type GithubUser {
-        avatar_url: String
-        company: String
-        id: String
-        login: String
-        repos: [GithubRepo]
-    }
+type GithubRepo {
+    id: String
+    name: String
+    commits(limit: Int): [GithubCommit]
+    branches(limit: Int): [GithubBranch]
+    owner: GithubUser
+}
+
+# Status of a commit
+type GithubStatus {
+    state: String
+    description: String
+    target_url: String
+    avatar_url: String
+    context: String
+    updated_at: String
+}
+
+type GithubUser {
+    login: String
+    id: String
+    company: String
+    avatar_url: String
+    repos: [GithubRepo]
+}
+
+type GraphQL_github_API {
+    github: GithubAPI
+}
+
+union UserOrCommitAuthor = GithubCommitAuthor | GithubUser
+
+type PullRequest {
+    title: String
+    number: Int!
+    url: String
+}
 `;
+    
