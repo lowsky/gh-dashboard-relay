fa0c492d4d8b29cbfe52857265213818266b5447
lowsky <lowsky@users.noreply.github.com>
2021-06-08 08:23:35 +0200

    5. use new relay based component

--- container/BranchInfoRow.tsx
+++ container/BranchInfoRow.tsx
@@ -4,21 +4,27 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons'
 
 import UILibContext from '../components/UILibContext';
 import { GithubBranch } from "../lib/types/resolvers";
+import PullRequestInfo from '../relay/PullRequestInfo';
 
 export interface BranchInfoRowProps {
   branch: GithubBranch;
 }
 
-const BranchInfoRow:React.FC<BranchInfoRowProps> = ({ branch }) => {
+const BranchInfoRow: React.FC<BranchInfoRowProps> = ({ branch }) => {
     const { name, lastCommit = {} } = branch;
+    const { associatedPullRequests } = lastCommit;
     const githubBranchSrc = `https://github.com/lowsky/dashboard/tree/${name}`;
     const { CommitWithStatuses } = useContext(UILibContext);
 
     return (
         <tr key={name}>
             <td>
-                <a href={githubBranchSrc}>{name}</a>{' '}
-                <FontAwesomeIcon icon={faGithub} />
+                <a href={githubBranchSrc}>{name}</a> <FontAwesomeIcon icon={faGithub} />
+            </td>
+            <td>
+                {associatedPullRequests?.map?.((pr, idx) => (
+                    <PullRequestInfo key={idx} pullRequest={pr} />
+                ))}
             </td>
             <td>
                 <CommitWithStatuses commit={lastCommit} />
