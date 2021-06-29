2c327a7b1348f8f819e1b52f8bdc57a9bf9938e9
lowsky <lowsky@users.noreply.github.com>
2021-06-29 19:31:16 +0200

    update next config

--- next.config.js
+++ next.config.js
@@ -2,9 +2,6 @@ const withImages = require('next-images')
 
 module.exports = {
   ...withImages(),
-  future: {
-    webpack5: true,
-  },
   // Target must be serverless (for use with netlify)
   target: "serverless",
 };
