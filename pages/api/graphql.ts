b2c597b79df57cf754604216c3cb2196a70b592e
lowsky <217931+lowsky@users.noreply.github.com>
2021-08-02 22:37:10 +0200

    Finally migrating to apollo server v3 (#1587)
    * adapt graphql server after migrating to apollo*3
    
    * adapt graphql server after migrating to apollo*3
    
    * chore(deps): fix/downgrade @types-react
    
    Co-authored-by: lowsky <lowsky@users.noreply.github.com>

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,22 +1,40 @@
+import { IncomingMessage, ServerResponse } from 'http'
 import { ApolloServer } from 'apollo-server-micro';
 
 import { typeDefs } from "../../lib/localSchema";
 import { resolvers } from '../../lib/resolvers';
+import {
+    ApolloServerPluginLandingPageDisabled,
+    ApolloServerPluginLandingPageGraphQLPlayground,
+} from 'apollo-server-core';
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
+// will be stored here for re-use
+let server: ApolloServer | null = null;
 
-    debug: true,
-    introspection: true,
-    playground: true,
-});
+async function getGraphqlServer() {
+    const apolloServer = new ApolloServer({
+        typeDefs,
+        resolvers,
+        debug: true,
+        introspection: true,
+        plugins: [
+            process.env.NODE_ENV === 'production'
+                ? ApolloServerPluginLandingPageDisabled()
+                : ApolloServerPluginLandingPageGraphQLPlayground(),
+        ],
+    });
 
-const handler = server.createHandler({
+    await apolloServer.start();
+    return apolloServer;
+}
 
-    path: '/api/graphql',
-});
-
+const handler = async (req: IncomingMessage, res: ServerResponse) => {
+    const apolloServer: ApolloServer = server || (await getGraphqlServer());
+    server = apolloServer;
+    return apolloServer.createHandler({
+        path: '/api/graphql',
+    })(req, res);
+};
 
 export const config = {
     api: {
