17851a20a3709ccb38eec6bb3e4851e207488e91
lowsky <lowsky@users.noreply.github.com>
2021-08-03 15:59:35 +0200

    fix serverless build, remove obsolete use of next-images
    failed on Error: ts-tiny-invariant not found

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
-const withImages = require('next-images')
-
 module.exports = {
-  ...withImages(),
+  // Error: Cannot find module 'ts-tiny-invariant'
+  // netlify is working on it...
+  webpack: (config, { isServer } ) => {
+    if (isServer) {
+      // When working on webpack5 there was this runtime-error:
+      // Module "ts-tiny-invariant" was not found. PR...
+      config.externals.push('ts-tiny-invariant')
+    }
+    return config
+  },
   // Target must be serverless (for use with netlify)
   target: "serverless",
 };
