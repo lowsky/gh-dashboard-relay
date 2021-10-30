27fbd842e2a46bdca4937cf13f0f82ac28dbabdc
lowsky <lowsky@users.noreply.github.com>
2021-10-30 17:44:38 +0200

    fix main unit test

--- stories/Pages.story.js
+++ stories/Pages.story.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react';
+import React, { Suspense } from 'react';
 
 import IndexPage from '../pages';
 import { RestfulMain } from '../pages/restful/[userName]/[repoName]';
