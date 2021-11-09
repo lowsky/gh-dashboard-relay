121fca095c83e60d6227e2d54024a224acda5feb
lowsky <lowsky@users.noreply.github.com>
2021-11-09 21:15:16 +0100

    remove extra loading relay context into every page

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,15 @@
-import 'bulma/css/bulma.css';
-
-import { RelayEnvironmentProvider } from 'react-relay/hooks';
-import { useEnvironment } from '../lib/relay';
-
 import { NavBar } from '../components/NavBar';
 
-import './main.css'
+import 'bulma/css/bulma.css';
+import './main.css';
 
 export default function App({ Component, pageProps }) {
-    const environment = useEnvironment(pageProps.initialRecords);
-
     return (
-        <RelayEnvironmentProvider environment={environment}>
+        <>
             <header>
                 <NavBar />
             </header>
             <Component {...pageProps} />
-        </RelayEnvironmentProvider>
+        </>
     );
 }
