c6ec3f6bb94b650601541bdcfa7f223b623ca670
lowsky <lowsky@users.noreply.github.com>
2021-11-18 21:06:05 +0100

    improve jest setup for coverage

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   verbose: true,
-  transform:{ "\\.[jt]sx?$": "babel-jest"},
+  transform: { "\\.[jt]sx?$": "babel-jest"},
+  "coveragePathIgnorePatterns": [
+    "node_modules",
+    ".storybook",
+    'relay/__generated__/',
+    ".story.js"
+  ],
   moduleNameMapper:{
     "\\.(css|less|scss)$": "identity-obj-proxy"
   },
