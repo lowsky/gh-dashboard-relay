35e75beebd94805f15ef727d9eb90216b9b73ba8
lowsky <lowsky@users.noreply.github.com>
2022-01-09 21:02:44 +0100

    upgrade eslint and do some clean-ups:
    * cleanup next.config,
    * format package.json
    * remove conflicting dependency @types/react
    * currently there is no storybook generated...

--- next.config.js
+++ next.config.js
@@ -1,24 +1,12 @@
 module.exports = {
-  // Error: Cannot find module 'ts-tiny-invariant'
-  // netlify is working on it...
-  webpack: (config, { isServer } ) => {
-    if (isServer) {
-      // When working on webpack5 there was this runtime-error:
-      // Module "ts-tiny-invariant" was not found. PR...
-      config.externals.push('ts-tiny-invariant')
-    }
-    return config
-  },
-  typescript: {
-    /* workaround for non-matching type definitions when
-       mixing apollo 2/3 ...
-     */
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
-  // Target must be serverless (for use with netlify)
-  target: "serverless",
+    // Error: Cannot find module 'ts-tiny-invariant'
+    // netlify is working on it...
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            // When working on webpack5 there was this runtime-error:
+            // Module "ts-tiny-invariant" was not found. PR...
+            config.externals.push('ts-tiny-invariant');
+        }
+        return config;
+    },
 };
