fc570be0de1694df1cd293ce39fc63689486d6fa
lowsky <lowsky@users.noreply.github.com>
2022-02-27 18:49:49 +0100

    remove redundant, generated js files

--- components/Spinner.story.js
+++
@@ -1,6 +0,0 @@
-import { Spinner } from './Spinner';
-import { CircularProgress, CircularProgressLabel } from '@chakra-ui/react';
-export default {
-    title: 'Others/Spinner',
-};
-export const Default = () => <Spinner />;
