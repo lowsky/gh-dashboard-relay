376689a65d35397f5af4ac64a2222cedb558c4b6
lowsky <lowsky@users.noreply.github.com>
2022-05-28 23:27:01 +0200

    replace apollo server with helix/envelop

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,38 +1,72 @@
-import { IncomingMessage, ServerResponse } from 'http';
-import { ApolloServer } from 'apollo-server-micro';
+/* eslint-disable react-hooks/rules-of-hooks */
+
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next';
+
+import {
+    getGraphQLParameters,
+    processRequest,
+    renderGraphiQL,
+    RenderGraphiQLOptions,
+    Request,
+    sendResult,
+    shouldRenderGraphiQL,
+} from 'graphql-helix';
+import { envelop, useSchema } from '@envelop/core';
+import { makeExecutableSchema } from '@graphql-tools/schema';
 
 import { typeDefs } from '../../lib/localSchema';
 import { resolvers } from '../../lib/resolvers';
 
 const isLocalDev = process.env.NODE_ENV === 'development';
 
-// will be stored here for re-use
-let server: ApolloServer | null = null;
-
-async function getGraphqlServer() {
-    const apolloServer = new ApolloServer({
-        typeDefs,
-        resolvers,
-        debug: isLocalDev,
-        introspection: isLocalDev,
+function getEnveloped(req) {
+    const schema = makeExecutableSchema({ typeDefs, resolvers });
+    const getEnveloped = envelop({
+        plugins: [useSchema(schema)],
     });
 
-    await apolloServer.start();
-    return apolloServer;
+    return getEnveloped({ req });
 }
 
-const handler = async (req: IncomingMessage, res: ServerResponse) => {
-    const apolloServer: ApolloServer = server || (await getGraphqlServer());
-    server = apolloServer;
-    return apolloServer.createHandler({
-        path: '/api/graphql',
-    })(req, res);
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { parse, validate, contextFactory, execute, schema } = getEnveloped(req);
+
+    const request: Request = {
+        body: req.body,
+        headers: req.headers,
+        // @ts-expect-error: TS2322: Type 'string | undefined' is not assignable to type 'string'.
+        method: req.method,
+        query: req.query,
+    };
+
+    await handleRequest(request, res, parse, validate, execute, contextFactory, schema);
 };
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
+async function handleRequest(request: Request, res: NextApiResponse, parse, validate, execute, contextFactory, schema) {
+    if (shouldRenderGraphiQL(request)) {
+        const options: RenderGraphiQLOptions = { endpoint: '/api/graphql' };
+        res.send(renderGraphiQL(options));
+        return;
+    }
+
+    const graphQLParameters = getGraphQLParameters(request);
+    const { operationName, query, variables } = graphQLParameters;
+
+    const result = await processRequest({
+        parse,
+        validate,
+        execute,
+        contextFactory,
+        operationName,
+        query,
+        variables,
+        request,
+        schema,
+    });
+    isLocalDev && console.log({ result });
+
+    await sendResult(result, res);
+}
 
 export default handler;
