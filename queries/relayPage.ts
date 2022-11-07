a775fdb72ef12a94b09149b07bd852768e884379
lowsky <lowsky@users.noreply.github.com>
2022-11-07 09:12:22 +0100

    make use of defer+Suspend

--- queries/relayPage.ts
+++ queries/relayPage.ts
@@ -3,14 +3,14 @@ import { graphql } from 'react-relay';
 export const userQuery = graphql`
     query relayPageUserQuery($userName: String!) {
         user(username: $userName) {
-            ...UserRepo_user
+            ...UserRepo_user @defer
         }
     }
 `;
 export const repoQuery = graphql`
     query relayPageRepoQuery($userName: String!, $repoName: String!) {
         repo(ownerUsername: $userName, name: $repoName) {
-            ...UserRepo_repo
+            ...UserRepo_repo @defer
         }
     }
 `;
