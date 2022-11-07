2eac337cdf968092ffdbda5a27c599af9f9c47b0
lowsky <lowsky@users.noreply.github.com>
2022-11-07 09:03:14 +0100

    replace relay network fetch function wor use of graphql defer, using meros lib

--- lib/relayNetwork.ts
+++ lib/relayNetwork.ts
@@ -1,28 +1,52 @@
-import { Network, Observable } from 'relay-runtime';
+import { FetchFunction, GraphQLResponse, Network, Observable } from 'relay-runtime';
+import { meros } from 'meros';
 
-import fetchMultipart from 'fetch-multipart-graphql';
+import type { ExecutionPatchResult } from 'graphql';
 
 // Define a function that fetches the results of an operation (query/mutation/etc.)
-// and returns its results as a Promise
-function fetchQuery(operation, variables) {
+const fetchQuery: FetchFunction = (params, variables, _cacheConfig) => {
     return Observable.create((sink) => {
-        fetchMultipart(process.env.NEXT_PUBLIC_RELAY_ENDPOINT ?? '/api/graphql', {
-            method: 'POST',
-            headers: {
-                Accept: 'application/json',
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                query: operation.text,
-                variables,
-            }),
-            credentials: 'same-origin',
+        (async () => {
+            const response = await fetch('/api/graphql', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    query: params.text,
+                    variables,
+                }),
+                credentials: 'same-origin',
+            });
+            const parts = await meros<ExecutionPatchResult>(response);
 
-            onNext: (parts) => sink.next(parts),
-            onError: (err) => sink.error(err as Error), //wrap it later
-            onComplete: () => sink.complete(),
-        });
+            if (isAsyncIterable(parts)) {
+                for await (const part of parts) {
+                    if (!part.json) {
+                        sink.error(new Error('Failed to parse part as json.'));
+                        break;
+                    }
+                    sink.next(part.body as GraphQLResponse);
+                }
+            } else {
+                sink.next(await parts.json());
+            }
+
+            sink.complete();
+        })();
     });
-}
+};
 
 export const network = Network.create(fetchQuery);
+
+// Type Guard
+function isAsyncIterable(input: unknown): input is AsyncIterable<unknown> {
+    return (
+        typeof input === 'object' &&
+        input !== null &&
+        // Some browsers still don't have Symbol.asyncIterator implemented (iOS Safari)
+        // That means every custom AsyncIterable must be built using a AsyncGeneratorFunction
+        // (async function * () {})
+        ((input as any)[Symbol.toStringTag] === 'AsyncGenerator' || Symbol.asyncIterator in input)
+    );
+}
