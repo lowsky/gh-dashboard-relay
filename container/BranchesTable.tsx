f406c02dc71cb51a51354c352d220bd3ebc6a9eb
lowsky <lowsky@users.noreply.github.com>
2022-11-17 10:13:23 +0100

    add loading spinner on each branch-row

--- container/BranchesTable.tsx
+++ container/BranchesTable.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Table, Tbody, Th, Thead, Tr } from '@chakra-ui/react';
+import React, { Suspense } from 'react';
+import { Table, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react';
 
 import { GithubRepo } from '../lib/types/resolvers';
 import { useUILib } from '../components/UILibContext';
 import { DoMergePR } from './UserRepo';
+import { Spinner } from '../components/Spinner';
 
 export interface BranchesTableProps {
     repo?: GithubRepo;
@@ -11,6 +12,9 @@ export interface BranchesTableProps {
 }
 const BranchesTable: React.FC<BranchesTableProps> = ({ repo, doMergePR }) => {
     const { BranchInfoRow } = useUILib();
+
+    const repoName = repo?.name;
+    const userName = repo?.owner?.login;
 
     return (
         <Table size="sm" variant="striped">
@@ -25,9 +29,34 @@ const BranchesTable: React.FC<BranchesTableProps> = ({ repo, doMergePR }) => {
                 </Tr>
             </Thead>
             <Tbody>
-                {(repo?.branches || []).map(
-                    (branch, idx) => branch && <BranchInfoRow key={idx} branch={branch} doMergePR={doMergePR} />
-                )}
+                {(repo?.branches || []).map((branch, idx) => {
+                    if (!branch?.lastCommit?.sha) return '0';
+                    return (
+                        branch && (
+                            <Suspense
+                                fallback={
+                                    <Tr>
+                                        <Td>
+                                            <Spinner />
+                                        </Td>
+                                        <Td>
+                                            <Spinner />
+                                        </Td>
+                                        <Td>
+                                            <Spinner />
+                                        </Td>
+                                    </Tr>
+                                }>
+                                <BranchInfoRow
+                                    key={idx}
+                                    branch={branch}
+                                    doMergePR={doMergePR}
+                                    {...{ userName, repoName, sha: branch.lastCommit.sha }}
+                                />
+                            </Suspense>
+                        )
+                    );
+                })}
             </Tbody>
         </Table>
     );
