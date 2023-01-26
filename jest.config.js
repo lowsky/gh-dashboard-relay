447d5b6bea061ce217c991db9b624b054ebb14cb
lowsky <lowsky@users.noreply.github.com>
2023-01-26 22:25:59 +0100

    remove obsolete babel dependency, run jest via next/swc

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,21 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: './' });
+
+// Any custom config you want to pass to Jest
+const customJestConfig = {
     verbose: true,
-    transform: { '\\.[jt]sx?$': 'babel-jest' },
-    coveragePathIgnorePatterns: ['node_modules', '.storybook', 'relay/__generated__/', '.story.js'],
-    moduleNameMapper: {
-        '\\.(css|less|scss)$': 'identity-obj-proxy',
-    },
+    coveragePathIgnorePatterns: [
+        //
+        'node_modules',
+        '.storybook',
+        'relay/__generated__/',
+        '.story.js',
+        '.story.tsx',
+    ],
     preset: 'ts-jest',
 };
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig);
