28dd8e9af0edc99d54262bd6edab618d55ee2fd1
lowsky <217931+lowsky@users.noreply.github.com>
2023-01-26 09:42:04 +0100

    upgrade storybook 7 (#2226)
    Co-authored-by: lowsky <lowsky@users.noreply.github.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,5 @@
 const path = require('path');
 module.exports = {
-    core: {
-        builder: 'webpack5',
-    },
     stories: [
         '../components/*.story.@(js|jsx|ts|tsx|mdx)',
         '../container/*.story.@(js|jsx|ts|tsx|mdx)',
@@ -10,41 +7,14 @@ module.exports = {
         '../relay/*.story.@(js|jsx|ts|tsx|mdx)',
         '../stories/*.story.@(js|jsx|ts|tsx|mdx)',
     ],
-    addons: [
-        //
-        '@storybook/addon-essentials',
-        '@chakra-ui/storybook-addon',
-        'storybook-addon-next-router',
-    ],
-    // sb build bails missing some deps if missing
-    typescript: { reactDocgen: false },
-    features: {
-        postcss: false,
+    addons: ['@storybook/addon-essentials', '@chakra-ui/storybook-addon'],
+    docs: {
+        autodocs: true,
     },
-    webpackFinal: async (config) => ({
-        ...config,
-        module: {
-            ...config.module,
-            rules: [
-                ...config.module.rules,
-                {
-                    type: 'javascript/auto',
-                    test: /\.mjs$/,
-                    include: /node_modules/,
-                },
-            ],
+    framework: {
+        name: '@storybook/nextjs',
+        options: {
+            fastRefresh: true,
         },
-        // Enable Docs pages
-        // https://github.com/storybookjs/storybook/issues/15253#issuecomment-951046559
-        resolve: {
-            ...config.resolve,
-            alias: {
-                ...config.resolve.alias,
-                // Resolve path issue for emotion v11
-                '@emotion/core': path.resolve('node_modules/@emotion/react'),
-                'emotion-theming': path.resolve('node_modules/@emotion/react'),
-            },
-        },
-    }),
-    framework: '@storybook/react',
+    },
 };
