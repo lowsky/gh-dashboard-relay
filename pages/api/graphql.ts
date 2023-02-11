4734aac62d57e7b0977b8d0aca99f3e722ca7152
lowsky <217931+lowsky@users.noreply.github.com>
2023-02-11 18:06:11 +0100

    switch to yoga (#2268)
    Co-authored-by: lowsky <lowsky@users.noreply.github.com>

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,69 +1,18 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from 'next';
-import { execute, parse, subscribe, validate } from 'graphql';
-import {
-    getGraphQLParameters,
-    processRequest,
-    renderGraphiQL,
-    RenderGraphiQLOptions,
-    Request,
-    sendResult,
-    shouldRenderGraphiQL,
-} from 'graphql-helix';
-import { envelop, useEngine, useSchema } from '@envelop/core';
+import { createYoga } from 'graphql-yoga';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
 import { typeDefs } from '../../lib/localSchema';
 import { resolvers } from '../../lib/resolvers';
 
-function getEnveloped(req) {
-    const schema = makeExecutableSchema({ typeDefs, resolvers });
-    const getEnveloped = envelop({
-        plugins: [useSchema(schema), useEngine({ parse, validate, execute, subscribe })],
-    });
+const schema = makeExecutableSchema({ typeDefs, resolvers });
 
-    return getEnveloped({ req });
-}
-
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const { parse, validate, contextFactory, execute, schema } = getEnveloped(req);
-
-    const request: Request = {
-        body: req.body,
-        headers: req.headers,
-        // @ts-expect-error: TS2322: Type 'string | undefined' is not assignable to type 'string'.
-        method: req.method,
-        query: req.query,
-    };
-
-    await handleRequest(request, res, parse, validate, execute, contextFactory, schema);
-};
-
-async function handleRequest(request: Request, res: NextApiResponse, parse, validate, execute, contextFactory, schema) {
-    if (shouldRenderGraphiQL(request)) {
-        const options: RenderGraphiQLOptions = { endpoint: '/api/graphql' };
-        res.send(renderGraphiQL(options));
-        return;
-    }
-
-    const graphQLParameters = getGraphQLParameters(request);
-    const { operationName, query, variables } = graphQLParameters;
-
-    const result = await processRequest({
-        parse,
-        validate,
-        execute,
-        contextFactory,
-        operationName,
-        query,
-        variables,
-        request,
-        schema,
-    });
-
-    await sendResult(result, res);
-}
-
-export default handler;
+export default createYoga<{
+    req: NextApiRequest;
+    res: NextApiResponse;
+}>({
+    schema,
+    logging: true,
+    graphqlEndpoint: '/api/graphql',
+    context: (_ctx) => ({}),
+});
