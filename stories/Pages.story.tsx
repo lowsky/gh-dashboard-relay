c59cd5c7b1b5b656832786bc902ca0a6a7b536ba
lowsky <lowsky@users.noreply.github.com>
2023-04-02 18:41:02 +0200

    disable storybook pages - story in chromatic

--- stories/Pages.story.tsx
+++ stories/Pages.story.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 
-// temporary disabled, until mdx is setup for storybook properly: import IndexPage from '../pages/index.mdx';
+// temporarily disabled, until mdx is set-up for storybook properly: import IndexPage from '../pages/index.mdx';
 import RestfulPage from '../pages/restful/[userName]/[repoName]';
+import { UserRepoFromUrlProvider } from '../components/useUserRepoFromRoute';
 
 // temporary disabled import { RelayRootMain } from'../pages/relay/[userName]/[repoName]';
 
@@ -14,15 +15,25 @@ export default {
 export const Restful = (props) => <RestfulPage {...props} />;
 Restful.parameters = {
     storyshots: { disable: true },
-    chromatic: { disable: false },
-    nextRouter: {
-        path: '/profile/[userName]/[repoName]',
-        query: {
-            userName: 'lowsky',
-            repoName: 'test-repo',
+    chromatic: { disable: true },
+    nextjs: {
+        appDirectory: false,
+        navigation: {
+            pathname: '/restful/[userName]/[repoName]',
+            query: {
+                userName: 'lowsky',
+                repoName: 'test-repo',
+            },
         },
     },
 };
+
+// still does not work
+Restful.decorators = [
+    (story) => {
+        return <UserRepoFromUrlProvider>{story}</UserRepoFromUrlProvider>;
+    },
+];
 
 /* this does currently not work at all, and need
 a different setup, with mocked Graphql Environment,
