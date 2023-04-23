4171733653955450d6c97b695a85b42d4bf08407
lowsky <lowsky@users.noreply.github.com>
2023-04-23 17:14:42 +0200

    hide CacheOverlay when not needed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,24 +4,44 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 import { themeConfig } from '../components/theme';
 import { NavBar } from '../components/NavBar';
-import DirectorLayout from '../components/DirectorLayout';
+import CacheDirectorLayout from '../components/DirectorLayout';
 import { UILibPureComponents } from '../components';
 import UILibContext from '../components/UILibContext';
+import { NextRouter, useRouter } from 'next/router';
 
 export const customTheme = extendTheme({ ...themeConfig });
 
+const routesWithCacheOverlay = [
+    '/wait-for-all',
+    '/waterfall',
+    '/side-by-side',
+    // ...
+];
+const matchRoutePrefix = (r: NextRouter) => (prefix) => r.pathname.startsWith(prefix);
+
 export default function App({ Component, pageProps }) {
+    const r = useRouter();
+    const routeWithCache = Boolean(routesWithCacheOverlay.find(matchRoutePrefix(r)));
+
     return (
         <ChakraProvider theme={customTheme}>
             <header>
                 <NavBar />
             </header>
 
-            <DirectorLayout>
+            {routeWithCache && (
+                <CacheDirectorLayout>
+                    <UILibContext.Provider value={UILibPureComponents}>
+                        <Component {...pageProps} />
+                    </UILibContext.Provider>
+                </CacheDirectorLayout>
+            )}
+
+            {!routeWithCache && (
                 <UILibContext.Provider value={UILibPureComponents}>
                     <Component {...pageProps} />
                 </UILibContext.Provider>
-            </DirectorLayout>
+            )}
         </ChakraProvider>
     );
 }
