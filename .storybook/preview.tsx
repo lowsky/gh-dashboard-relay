48cf9ebe9a5e37705c59fca4349fcbea0fe45638
lowsky <217931+lowsky@users.noreply.github.com>
2023-06-08 16:21:53 +0200

    Chakra-UI is currently not fully compatible with latest storybook (#2401)
    Co-authored-by: lowsky <lowsky@users.noreply.github.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,10 @@
+import React, { useMemo } from 'react';
 import { RouterContext } from 'next/dist/shared/lib/router-context';
-import { themeConfig } from '../components/theme';
-
+import type { DecoratorFn } from '@storybook/react';
+import { ChakraProvider } from '@chakra-ui/react';
 import { extendTheme } from '@chakra-ui/react';
+
+import { themeConfig } from '../components/theme';
 
 export const parameters = {
     actions: { argTypesRegex: '^on[A-Z].*' },
@@ -12,3 +15,23 @@ export const parameters = {
         theme: extendTheme({ ...themeConfig }),
     },
 };
+
+export const ChakraProviderDecorator: DecoratorFn = (Story, context) => {
+    const {
+        parameters: { chakra: chakraParams },
+    } = context;
+    const chakraTheme = chakraParams?.theme
+        ? typeof chakraParams.theme === 'function'
+            ? chakraParams.theme(context)
+            : chakraParams.theme
+        : {}; // TODO What is the default theme?
+    // TODO const direction = useDirection(globalDirection || chakraTheme?.direction);
+    const themeWithDirectionOverride = useMemo(() => extendTheme(chakraTheme), [chakraTheme]);
+    return (
+        <ChakraProvider {...chakraParams} theme={themeWithDirectionOverride}>
+            <Story {...context} />
+        </ChakraProvider>
+    );
+};
+
+export const decorators = [ChakraProviderDecorator];
