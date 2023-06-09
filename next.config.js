3011e51d6207b23e39ec43b7a4f64ffdb6629f00
lowsky <lowsky@users.noreply.github.com>
2023-06-09 21:10:34 +0200

    remove obsolete workaround

--- next.config.js
+++ next.config.js
@@ -16,17 +16,6 @@ module.exports = {
         relay,
     },
     ...withMDX({
-        // Error: Cannot find module 'ts-tiny-invariant'
-        // netlify is working on it...
-        webpack: (config, { isServer }) => {
-            if (isServer) {
-                // When working on webpack5 there was this runtime-error:
-                // Module "ts-tiny-invariant" was not found. PR...
-                config.externals.push('ts-tiny-invariant');
-            }
-            return config;
-        },
-        // Append the default value with md extensions
         pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     }),
 };
