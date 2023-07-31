157365adf25b29fa0886e453e7d3d88765480721
lowsky <lowsky@users.noreply.github.com>
2023-07-31 21:02:41 +0200

    migrate storybook config to TS

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,26 +1,27 @@
-module.exports = {
+import { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
     stories: [
         '../components/*.story.@(js|jsx|ts|tsx|mdx)',
         '../container/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../pages/*.story.@(js|jsx|ts|tsx|mdx)',
-        '../relay/*.story.@(js|jsx|ts|tsx|mdx)',
         '../stories/*.story.@(js|jsx|ts|tsx|mdx)',
     ],
     addons: [
         '@storybook/addon-essentials',
         '@chakra-ui/storybook-addon',
     ],
-    features: {
-        emotionAlias: false,
-    },
     docs: {
         autodocs: true,
+    },
+    core: {
+        disableTelemetry:true,
     },
     framework: {
         name: '@storybook/nextjs',
         options: {
-
             fastRefresh: true,
         },
     },
 };
+
+export default config;
