98827a91592dd55dc4267ecdf1d1d7b948cf7821
lowsky <lowsky@users.noreply.github.com>
2023-08-06 16:56:23 +0200

    remove obsolete UILibClientWrapper.tsx

--- components/UILibClientWrapper.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import { UILib, UILibPureComponents } from "./UILibContext";
-
-export function UILibClientWrapper({ children }) {
-    return <UILib value={UILibPureComponents}>{children}</UILib>;
-}
