49136ea0cb4e60fc7303f4d185c241b7a170a5c6
lowsky <lowsky@users.noreply.github.com>
2023-08-06 15:36:26 +0200

    add storybook files in subfolders

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,10 @@ import { StorybookConfig } from '@storybook/nextjs';
 const config: StorybookConfig = {
     stories: [
         '../components/*.story.@(js|jsx|ts|tsx|mdx)',
+        '../components/**/*.story.@(js|jsx|ts|tsx|mdx)',
         '../container/*.story.@(js|jsx|ts|tsx|mdx)',
+        '../container/**/*.story.@(js|jsx|ts|tsx|mdx)',
+        '../app/*.story.@(js|jsx|ts|tsx|mdx)',
         '../app/**/*.story.@(js|jsx|ts|tsx|mdx)',
     ],
     addons: [
