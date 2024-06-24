904ecab30e8b7d36df285223fd24f28d62271c26
lowsky <lowsky@users.noreply.github.com>
2024-06-24 09:40:19 +0200

    fix GH oauth redirect url

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -2,8 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function login(_req: NextApiRequest, res: NextApiResponse) {
     const client_id = process.env.GH_BASIC_CLIENT_ID;
-    const redirect_uri = `${process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000'}/api/auth/callback`;
+    const next_url = process.env.NEXT_PUBLIC_VERCEL_URL;
+    const redirect_uri = next_url?.startsWith('http') ? next_url : `https://${next_url}/api/auth/callback`;
     const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`;
 
-    res.redirect(githubAuthUrl);
+    console.log('redirecting to ' + githubAuthUrl);
+
+    if (next_url) res.redirect(githubAuthUrl);
+    else console.error('Sorry, redirect-url-base on cannot be undefined, please check env: NEXT_PUBLIC_VERCEL_URL');
 }
