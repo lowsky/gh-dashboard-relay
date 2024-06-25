cc66e0b442fc8c6759d4bc9e5e5e903ebb31135d
lowsky <lowsky@users.noreply.github.com>
2024-06-25 20:22:20 +0200

    remove fetching user banner

--- components/WarningGitHubRateLimiting.tsx
+++ components/WarningGitHubRateLimiting.tsx
@@ -1,28 +1,16 @@
 'use client'; // using useState
 
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { Alert, AlertDescription, AlertIcon } from '@chakra-ui/react';
 
-import { Login, Logout } from './Login';
-
-type FetchingUserInfo = {
-    name?: string;
-    loading?: true;
-    error?: string | unknown;
-} | null;
+import { Login } from './Login';
 
 export default function WarningGitHubRateLimiting() {
-    const [user, setUser] = useState<FetchingUserInfo>(null);
-
     return (
-        <Alert status={user?.loading ? 'loading' : user?.error ? 'error' : user?.name ? 'success' : 'info'}>
+        <Alert status="info">
             <AlertIcon />
             <AlertDescription>
-                {
-                    <>
-                        <Login /> to avoid GitHub rate-limiting and having access to your repositories!
-                    </>
-                }
+                <Login /> to avoid GitHub rate-limiting and having access to your repositories!
             </AlertDescription>
         </Alert>
     );
