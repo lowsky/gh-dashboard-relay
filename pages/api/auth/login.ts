f1e1e4ad29213fcd13ffc6620547b27df4c134cb
lowsky <lowsky@users.noreply.github.com>
2024-07-01 21:46:51 +0200

    request access to repo scope, when logging in

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -4,10 +4,11 @@ export default function login(_req: NextApiRequest, res: NextApiResponse) {
     const client_id = process.env.GH_BASIC_CLIENT_ID;
     const next_url = process.env.NEXT_PUBLIC_VERCEL_URL;
     const redirect_uri = next_url?.startsWith('http') ? next_url : `https://${next_url}/api/auth/callback`;
-    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`;
+    const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=repo`;
 
-    console.log('redirecting to ' + githubAuthUrl);
-
-    if (next_url) res.redirect(githubAuthUrl);
-    else console.error('Sorry, redirect-url-base on cannot be undefined, please check env: NEXT_PUBLIC_VERCEL_URL');
+    if (next_url) {
+        res.redirect(githubAuthUrl);
+    } else {
+        console.error('Sorry, redirect-url-base on cannot be undefined, please check env: NEXT_PUBLIC_VERCEL_URL');
+    }
 }
