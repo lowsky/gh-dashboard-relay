9f7af73ba03774e8c21794cfade42a7154d0c18a
lowsky <lowsky@users.noreply.github.com>
2024-07-03 09:07:08 +0200

    hide cookie value

--- components/WhenSignedIn.tsx
+++ components/WhenSignedIn.tsx
@@ -11,14 +11,10 @@ const WhenSignedIn = async (props: Props) => {
     const access_token = cookies().getAll('access_token') ?? [];
     const hasAccessToken = access_token.length > 0;
     if (hasAccessToken) {
-        const cookie = access_token[0];
-        return (
-            <>
-                <p>Name: {cookie.name}</p>
-                <p>Value: {cookie.value}</p>
-                {props.children}
-            </>
-        );
+        //const cookie = access_token[0];
+        // <p>Name: {cookie.name}</p>
+        // <p>Value: {cookie.value}</p>
+        return <>{props.children}</>;
     }
     return <p>No valid GitHub access-token! Please, sign-in !</p>;
 };
