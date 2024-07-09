d9ecceaa31194ccc07eaa7059baf1e88f7fc419a
lowsky <lowsky@users.noreply.github.com>
2024-07-09 23:58:22 +0200

    rework nextjs based page, split off error, layout, loading

--- app/next/[userName]/[repoName]/page.tsx
+++ app/next/[userName]/[repoName]/page.tsx
@@ -6,9 +6,7 @@ import React from 'react';
 import { getAuthorizedGitHub, User } from 'restinpeace/github';
 
 import { RepoType } from 'components/Repo';
-import { AsyncUserRepo } from 'container/AsyncUserRepo';
-import { UserRepoFromUrlProvider } from 'components/useUserRepoFromRoute';
-import InternalLink from 'components/InternalLink';
+import UserRepo from 'container/UserRepo';
 
 interface Props {
     userData: Promise<User>;
@@ -30,20 +28,9 @@ export default async function Page(props) {
 
     const userData = fetchUserPromise(userName);
     const repoData = fetchRepoBranches({ userName, repoName });
-
-    return (
-        <>
-            <InternalLink href="/next">Back to shortcut list</InternalLink>
-            <UserRepoFromUrlProvider>
-                <ReactNext userData={userData} repoData={repoData} userName={userName} repoName={repoName}/>
-            </UserRepoFromUrlProvider>
-        </>
-    );
 }
 
-async function ReactNext({ repoData, userData, userName, repoName }: Props) {
-    // @ts-expect-error TS2786: Its return type 'Promise<Element>' is not a valid JSX element.
-    return <AsyncUserRepo userData={userData} repoData={repoData} userName={userName} repoName={repoName}/>;
+    return <UserRepo user={userData} repo={repoData} />;
 }
 
 const fetchUserPromise: (userName) => Promise<User> = async (userName) => {
