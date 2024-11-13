b8fae997979a4304a73d2ce941e846a7a21c2631
lowsky <lowsky@users.noreply.github.com>
2024-11-13 22:06:32 +0100

    introduce RelayClient Context for use on client

--- app/relay/[userName]/[repoName]/page.tsx
+++ app/relay/[userName]/[repoName]/page.tsx
@@ -1,33 +1,24 @@
-'use client';
 import React, { Suspense } from 'react';
 
-// using context only works on the client: react-relay/lib/relay-hooks/_ProfilerContext_
-import { RelayEnvironmentProvider } from 'react-relay';
-import { IEnvironment } from 'relay-runtime';
-
+import RelayClientContext from 'app/relay/[userName]/[repoName]/RelayClientContext';
 import { RelayRootMain } from './relayRootMain';
 import RichErrorBoundary from 'components/RichErrorBoundary';
 import { ContentLoadingFallback } from 'components/ContentLoadingFallback';
-import { useEnvironment } from 'lib/relay';
 
 import { UserRepoFromUrlProvider } from 'components/useUserRepoFromRoute';
 import InternalLink from 'components/InternalLink';
 
 const RelayRoot = () => {
-    const environment: IEnvironment = useEnvironment({});
-
     return (
         <UserRepoFromUrlProvider>
             <InternalLink href="/relay">back to shortcut list</InternalLink>
-            {
-                <RelayEnvironmentProvider environment={environment}>
-                    <RichErrorBoundary>
-                        <Suspense fallback={<ContentLoadingFallback />}>
-                            <RelayRootMain />
-                        </Suspense>
-                    </RichErrorBoundary>
-                </RelayEnvironmentProvider>
-            }
+            <RelayClientContext>
+                <RichErrorBoundary>
+                    <Suspense fallback={<ContentLoadingFallback />}>
+                        <RelayRootMain />
+                    </Suspense>
+                </RichErrorBoundary>
+            </RelayClientContext>
         </UserRepoFromUrlProvider>
     );
 };
