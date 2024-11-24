e64fae89af36bff839b713d1a73eaae0ac537831
lowsky <lowsky@users.noreply.github.com>
2024-11-24 23:20:00 +0100

    fix build errors

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -19,17 +19,23 @@ export const Tooltip = React.forwardRef<HTMLDivElement, TooltipProps>(
 
         return (
             <ChakraTooltip.Root {...rest}>
-                <ChakraTooltip.Trigger asChild>{children}</ChakraTooltip.Trigger>
+                {
+                    // @ts-expect-error snippet type error
+                    <ChakraTooltip.Trigger asChild>{children}</ChakraTooltip.Trigger>
+                }
                 <Portal disabled={!portalled} container={portalRef}>
                     <ChakraTooltip.Positioner>
-                        <ChakraTooltip.Content ref={ref} {...contentProps}>
-                            {showArrow && (
-                                <ChakraTooltip.Arrow>
-                                    <ChakraTooltip.ArrowTip />
-                                </ChakraTooltip.Arrow>
-                            )}
-                            {content}
-                        </ChakraTooltip.Content>
+                        {
+                            // @ts-expect-error snippet type error
+                            <ChakraTooltip.Content ref={ref} {...contentProps}>
+                                {showArrow && (
+                                    <ChakraTooltip.Arrow>
+                                        <ChakraTooltip.ArrowTip />
+                                    </ChakraTooltip.Arrow>
+                                )}
+                                {content}
+                            </ChakraTooltip.Content>
+                        }
                     </ChakraTooltip.Positioner>
                 </Portal>
             </ChakraTooltip.Root>
