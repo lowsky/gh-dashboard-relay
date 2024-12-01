5385063d11fb37dc09c410929c6f998a15f1773d
lowsky <lowsky@users.noreply.github.com>
2024-12-01 15:40:43 +0100

    remove revalidate route

--- relay/PullRequestInfo.tsx
+++ relay/PullRequestInfo.tsx
@@ -3,7 +3,6 @@ import { graphql, useFragment } from 'react-relay';
 import { useUserRepo } from 'components/useUserRepoFromRoute';
 import PullRequestMerge, { type DoMergePR } from 'components/PullRequestMerge';
 import doMergePRAction from 'app/actions/doMergePRAction';
-import { revalidateCache } from 'app/actions/doRevalidateCache';
 
 export default function PRFragmentContainer(props) {
     const pullRequest = useFragment(
@@ -25,7 +24,6 @@ export default function PRFragmentContainer(props) {
 
     const doMergePR: DoMergePR = async () => {
         const mergeResult = doMergePRAction(pullRequest.number, userName, repoName, sha);
-        await revalidateCache({ pathPrefix: 'relay', userName, repoName });
         return mergeResult;
     };
 
