1fecb709aa3675f1d92468842ea6cb489aed76a1
lowsky <lowsky@users.noreply.github.com>
2025-05-14 09:34:10 +0200

    regenerate relay glue-code

--- relay/__generated__/PullRequestInfo_pullRequest.graphql.ts
+++ relay/__generated__/PullRequestInfo_pullRequest.graphql.ts
@@ -9,71 +9,74 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type PullRequestInfo_pullRequest$data = {
-  readonly headRef: {
-    readonly id: string;
-  } | null | undefined;
-  readonly number: number;
-  readonly title: string;
-  readonly url: any;
-  readonly " $fragmentType": "PullRequestInfo_pullRequest";
+    readonly headRef:
+        | {
+              readonly id: string;
+          }
+        | null
+        | undefined;
+    readonly number: number;
+    readonly title: string;
+    readonly url: any;
+    readonly ' $fragmentType': 'PullRequestInfo_pullRequest';
 };
 export type PullRequestInfo_pullRequest$key = {
-  readonly " $data"?: PullRequestInfo_pullRequest$data;
-  readonly " $fragmentSpreads": FragmentRefs<"PullRequestInfo_pullRequest">;
+    readonly ' $data'?: PullRequestInfo_pullRequest$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'PullRequestInfo_pullRequest'>;
 };
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "PullRequestInfo_pullRequest",
-  "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "title",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "number",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "url",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "Ref",
-      "kind": "LinkedField",
-      "name": "headRef",
-      "plural": false,
-      "selections": [
+    argumentDefinitions: [],
+    kind: 'Fragment',
+    metadata: null,
+    name: 'PullRequestInfo_pullRequest',
+    selections: [
         {
-          "alias": null,
-          "args": null,
-          "kind": "ScalarField",
-          "name": "id",
-          "storageKey": null
-        }
-      ],
-      "storageKey": null
-    }
-  ],
-  "type": "PullRequest",
-  "abstractKey": null
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'title',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'number',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'url',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            concreteType: 'Ref',
+            kind: 'LinkedField',
+            name: 'headRef',
+            plural: false,
+            selections: [
+                {
+                    alias: null,
+                    args: null,
+                    kind: 'ScalarField',
+                    name: 'id',
+                    storageKey: null,
+                },
+            ],
+            storageKey: null,
+        },
+    ],
+    type: 'PullRequest',
+    abstractKey: null,
 };
 
-(node as any).hash = "9a56a094fa56a7936dfe1cf0e398f89b";
+(node as any).hash = '9a56a094fa56a7936dfe1cf0e398f89b';
 
 export default node;
