edb45b426a7b83d6ba4183f7134b4ea7b491c931
lowsky <lowsky@users.noreply.github.com>
2025-05-14 21:21:04 +0200

    remove obsolete files (thanks knip!)

--- components/singleArgOrDefault.ts
+++
@@ -1,9 +0,0 @@
-export function singleArgOrDefault(value: string | string[], defaultValue: string) {
-    if (value === null || value === undefined) {
-        return defaultValue;
-    }
-    if (typeof value === 'string') {
-        return value;
-    }
-    return String(value);
-}
