a4c4dca102d3bc2ee9d18dbcc3bff1682be6b73c
lowsky <lowsky@users.noreply.github.com>
2025-05-14 09:33:00 +0200

    adopt storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -25,7 +25,7 @@ const config: StorybookConfig = {
     framework: {
         name: '@storybook/nextjs',
         options: {
-            fastRefresh: true,
+            lazyCompilation: true,
         },
     },
     webpackFinal: (baseConfig, _options) => {
