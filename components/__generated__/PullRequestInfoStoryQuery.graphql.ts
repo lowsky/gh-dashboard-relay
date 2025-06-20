39cc98202b743ca41df248230c49b0ff2ee5d8bd
lowsky <lowsky@users.noreply.github.com>
2025-06-20 18:29:51 +0200

    reformatting generated glue code

--- components/__generated__/PullRequestInfoStoryQuery.graphql.ts
+++ components/__generated__/PullRequestInfoStoryQuery.graphql.ts
@@ -9,167 +9,170 @@
 // @ts-nocheck
 
 import { ConcreteRequest } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type PullRequestInfoStoryQuery$variables = Record<PropertyKey, never>;
 export type PullRequestInfoStoryQuery$data = {
-  readonly node: {
-    readonly " $fragmentSpreads": FragmentRefs<"PullRequestMergeFragment_ref">;
-  } | null | undefined;
+    readonly node:
+        | {
+              readonly ' $fragmentSpreads': FragmentRefs<'PullRequestMergeFragment_ref'>;
+          }
+        | null
+        | undefined;
 };
 export type PullRequestInfoStoryQuery = {
-  response: PullRequestInfoStoryQuery$data;
-  variables: PullRequestInfoStoryQuery$variables;
+    response: PullRequestInfoStoryQuery$data;
+    variables: PullRequestInfoStoryQuery$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "kind": "Literal",
-    "name": "id",
-    "value": "test-id"
-  }
-];
-return {
-  "fragment": {
-    "argumentDefinitions": [],
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "PullRequestInfoStoryQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v0/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "node",
-        "plural": false,
-        "selections": [
-          {
-            "args": null,
-            "kind": "FragmentSpread",
-            "name": "PullRequestMergeFragment_ref"
-          }
-        ],
-        "storageKey": "node(id:\"test-id\")"
-      }
-    ],
-    "type": "Query",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": [],
-    "kind": "Operation",
-    "name": "PullRequestInfoStoryQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v0/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "node",
-        "plural": false,
-        "selections": [
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "__typename",
-            "storageKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "id",
-            "storageKey": null
-          },
-          {
-            "kind": "InlineFragment",
-            "selections": [
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "headRefOid",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "number",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "url",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "title",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "mergeStateStatus",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "closed",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "isDraft",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "isInMergeQueue",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "mergeable",
-                "storageKey": null
-              }
+const node: ConcreteRequest = (function () {
+    var v0 = [
+        {
+            kind: 'Literal',
+            name: 'id',
+            value: 'test-id',
+        },
+    ];
+    return {
+        fragment: {
+            argumentDefinitions: [],
+            kind: 'Fragment',
+            metadata: null,
+            name: 'PullRequestInfoStoryQuery',
+            selections: [
+                {
+                    alias: null,
+                    args: v0 /*: any*/,
+                    concreteType: null,
+                    kind: 'LinkedField',
+                    name: 'node',
+                    plural: false,
+                    selections: [
+                        {
+                            args: null,
+                            kind: 'FragmentSpread',
+                            name: 'PullRequestMergeFragment_ref',
+                        },
+                    ],
+                    storageKey: 'node(id:"test-id")',
+                },
             ],
-            "type": "PullRequest",
-            "abstractKey": null
-          }
-        ],
-        "storageKey": "node(id:\"test-id\")"
-      }
-    ]
-  },
-  "params": {
-    "cacheID": "9092bae7611971ee9e67abf04f06c835",
-    "id": null,
-    "metadata": {},
-    "name": "PullRequestInfoStoryQuery",
-    "operationKind": "query",
-    "text": "query PullRequestInfoStoryQuery {\n  node(id: \"test-id\") {\n    __typename\n    ...PullRequestMergeFragment_ref\n    id\n  }\n}\n\nfragment PullRequestMergeFragment_ref on PullRequest {\n  id\n  headRefOid\n  number\n  url\n  title\n  mergeStateStatus\n  closed\n  isDraft\n  isInMergeQueue\n  mergeable\n}\n"
-  }
-};
+            type: 'Query',
+            abstractKey: null,
+        },
+        kind: 'Request',
+        operation: {
+            argumentDefinitions: [],
+            kind: 'Operation',
+            name: 'PullRequestInfoStoryQuery',
+            selections: [
+                {
+                    alias: null,
+                    args: v0 /*: any*/,
+                    concreteType: null,
+                    kind: 'LinkedField',
+                    name: 'node',
+                    plural: false,
+                    selections: [
+                        {
+                            alias: null,
+                            args: null,
+                            kind: 'ScalarField',
+                            name: '__typename',
+                            storageKey: null,
+                        },
+                        {
+                            alias: null,
+                            args: null,
+                            kind: 'ScalarField',
+                            name: 'id',
+                            storageKey: null,
+                        },
+                        {
+                            kind: 'InlineFragment',
+                            selections: [
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'headRefOid',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'number',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'url',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'title',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'mergeStateStatus',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'closed',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'isDraft',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'isInMergeQueue',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'mergeable',
+                                    storageKey: null,
+                                },
+                            ],
+                            type: 'PullRequest',
+                            abstractKey: null,
+                        },
+                    ],
+                    storageKey: 'node(id:"test-id")',
+                },
+            ],
+        },
+        params: {
+            cacheID: '9092bae7611971ee9e67abf04f06c835',
+            id: null,
+            metadata: {},
+            name: 'PullRequestInfoStoryQuery',
+            operationKind: 'query',
+            text: 'query PullRequestInfoStoryQuery {\n  node(id: "test-id") {\n    __typename\n    ...PullRequestMergeFragment_ref\n    id\n  }\n}\n\nfragment PullRequestMergeFragment_ref on PullRequest {\n  id\n  headRefOid\n  number\n  url\n  title\n  mergeStateStatus\n  closed\n  isDraft\n  isInMergeQueue\n  mergeable\n}\n',
+        },
+    };
 })();
 
-(node as any).hash = "62dc7e6471a86f5dbc521a1f97265a99";
+(node as any).hash = '62dc7e6471a86f5dbc521a1f97265a99';
 
 export default node;
--- relay/__generated__/PullRequestInfo_pullRequest.graphql.ts
+++ relay/__generated__/PullRequestInfo_pullRequest.graphql.ts
@@ -9,66 +9,66 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type PullRequestInfo_pullRequest$data = {
-  readonly headRefOid: any;
-  readonly id: string;
-  readonly number: number;
-  readonly title: string;
-  readonly url: any;
-  readonly " $fragmentType": "PullRequestInfo_pullRequest";
+    readonly headRefOid: any;
+    readonly id: string;
+    readonly number: number;
+    readonly title: string;
+    readonly url: any;
+    readonly ' $fragmentType': 'PullRequestInfo_pullRequest';
 };
 export type PullRequestInfo_pullRequest$key = {
-  readonly " $data"?: PullRequestInfo_pullRequest$data;
-  readonly " $fragmentSpreads": FragmentRefs<"PullRequestInfo_pullRequest">;
+    readonly ' $data'?: PullRequestInfo_pullRequest$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'PullRequestInfo_pullRequest'>;
 };
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "PullRequestInfo_pullRequest",
-  "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "id",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "title",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "number",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "url",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "headRefOid",
-      "storageKey": null
-    }
-  ],
-  "type": "PullRequest",
-  "abstractKey": null
+    argumentDefinitions: [],
+    kind: 'Fragment',
+    metadata: null,
+    name: 'PullRequestInfo_pullRequest',
+    selections: [
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'id',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'title',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'number',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'url',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'headRefOid',
+            storageKey: null,
+        },
+    ],
+    type: 'PullRequest',
+    abstractKey: null,
 };
 
-(node as any).hash = "a4db669712822a32e610ea79a2566e53";
+(node as any).hash = 'a4db669712822a32e610ea79a2566e53';
 
 export default node;
