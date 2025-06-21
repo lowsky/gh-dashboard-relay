6530a2d4fcb231945a677646d67c2d8b60f4a297
lowsky <lowsky@users.noreply.github.com>
2025-06-21 19:12:03 +0200

    fix type issue with ReactNode

--- lib/RelayClientContext.tsx
+++ lib/RelayClientContext.tsx
@@ -6,5 +6,14 @@ import { getCurrentEnvironment } from './relay';
 export default function RelayClientContext({ auth, children }: { auth: string; children: ReactNode }) {
     const environment = getCurrentEnvironment(auth);
 
-    return <RelayEnvironmentProvider environment={environment}>{children}</RelayEnvironmentProvider>;
+    /*
+    Type error: Type 'React.ReactNode' is not assignable to type 'import("/Users/hostlows/github/relay-gh-cockpit/node_modules/@types/react-relay/node_modules/@types/react/index").ReactNode'.
+  Type 'ReactElement<unknown, string | JSXElementConstructor<any>>' is not assignable to type 'ReactNode'.
+    Property 'children' is missing in type 'ReactElement<unknown, string | JSXElementConstructor<any>>' but required in type 'ReactPortal'.
+     */
+    return (
+        <RelayEnvironmentProvider environment={environment}>
+            <>{children}</>
+        </RelayEnvironmentProvider>
+    );
 }
