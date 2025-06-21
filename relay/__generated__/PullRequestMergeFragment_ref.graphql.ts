c6c29305c9252619fafc360b2936c29b2cda0683
lowsky <lowsky@users.noreply.github.com>
2025-06-21 19:22:02 +0200

    fix formatting

--- relay/__generated__/PullRequestMergeFragment_ref.graphql.ts
+++ relay/__generated__/PullRequestMergeFragment_ref.graphql.ts
@@ -9,99 +9,108 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-export type MergeStateStatus = "BEHIND" | "BLOCKED" | "CLEAN" | "DIRTY" | "DRAFT" | "HAS_HOOKS" | "UNKNOWN" | "UNSTABLE" | "%future added value";
-import { FragmentRefs } from "relay-runtime";
+export type MergeStateStatus =
+    | 'BEHIND'
+    | 'BLOCKED'
+    | 'CLEAN'
+    | 'DIRTY'
+    | 'DRAFT'
+    | 'HAS_HOOKS'
+    | 'UNKNOWN'
+    | 'UNSTABLE'
+    | '%future added value';
+import { FragmentRefs } from 'relay-runtime';
 export type PullRequestMergeFragment_ref$data = {
-  readonly closed: boolean;
-  readonly headRefOid: any;
-  readonly id: string;
-  readonly isDraft: boolean;
-  readonly isInMergeQueue: boolean;
-  readonly mergeStateStatus: MergeStateStatus;
-  readonly number: number;
-  readonly title: string;
-  readonly url: any;
-  readonly " $fragmentType": "PullRequestMergeFragment_ref";
+    readonly closed: boolean;
+    readonly headRefOid: any;
+    readonly id: string;
+    readonly isDraft: boolean;
+    readonly isInMergeQueue: boolean;
+    readonly mergeStateStatus: MergeStateStatus;
+    readonly number: number;
+    readonly title: string;
+    readonly url: any;
+    readonly ' $fragmentType': 'PullRequestMergeFragment_ref';
 };
 export type PullRequestMergeFragment_ref$key = {
-  readonly " $data"?: PullRequestMergeFragment_ref$data;
-  readonly " $fragmentSpreads": FragmentRefs<"PullRequestMergeFragment_ref">;
+    readonly ' $data'?: PullRequestMergeFragment_ref$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'PullRequestMergeFragment_ref'>;
 };
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "PullRequestMergeFragment_ref",
-  "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "id",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "headRefOid",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "number",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "url",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "title",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "mergeStateStatus",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "closed",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "isDraft",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "isInMergeQueue",
-      "storageKey": null
-    }
-  ],
-  "type": "PullRequest",
-  "abstractKey": null
+    argumentDefinitions: [],
+    kind: 'Fragment',
+    metadata: null,
+    name: 'PullRequestMergeFragment_ref',
+    selections: [
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'id',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'headRefOid',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'number',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'url',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'title',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'mergeStateStatus',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'closed',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'isDraft',
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'isInMergeQueue',
+            storageKey: null,
+        },
+    ],
+    type: 'PullRequest',
+    abstractKey: null,
 };
 
-(node as any).hash = "2b6e6351cf2a0f3eab82ad5d9bda73dd";
+(node as any).hash = '2b6e6351cf2a0f3eab82ad5d9bda73dd';
 
 export default node;
--- relay/__generated__/UserFragmentStoryQuery.graphql.ts
+++ relay/__generated__/UserFragmentStoryQuery.graphql.ts
@@ -9,167 +9,170 @@
 // @ts-nocheck
 
 import { ConcreteRequest } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type UserFragmentStoryQuery$variables = Record<PropertyKey, never>;
 export type UserFragmentStoryQuery$data = {
-  readonly node: {
-    readonly " $fragmentSpreads": FragmentRefs<"UserFragment_user">;
-  } | null | undefined;
+    readonly node:
+        | {
+              readonly ' $fragmentSpreads': FragmentRefs<'UserFragment_user'>;
+          }
+        | null
+        | undefined;
 };
 export type UserFragmentStoryQuery = {
-  response: UserFragmentStoryQuery$data;
-  variables: UserFragmentStoryQuery$variables;
+    response: UserFragmentStoryQuery$data;
+    variables: UserFragmentStoryQuery$variables;
 };
 
-const node: ConcreteRequest = (function(){
-var v0 = [
-  {
-    "kind": "Literal",
-    "name": "id",
-    "value": "test-id"
-  }
-],
-v1 = {
-  "enumValues": null,
-  "nullable": false,
-  "plural": false,
-  "type": "String"
-};
-return {
-  "fragment": {
-    "argumentDefinitions": [],
-    "kind": "Fragment",
-    "metadata": null,
-    "name": "UserFragmentStoryQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v0/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "node",
-        "plural": false,
-        "selections": [
-          {
-            "kind": "InlineFragment",
-            "selections": [
-              {
-                "args": null,
-                "kind": "FragmentSpread",
-                "name": "UserFragment_user"
-              }
-            ],
-            "type": "User",
-            "abstractKey": null
-          }
+const node: ConcreteRequest = (function () {
+    var v0 = [
+            {
+                kind: 'Literal',
+                name: 'id',
+                value: 'test-id',
+            },
         ],
-        "storageKey": "node(id:\"test-id\")"
-      }
-    ],
-    "type": "Query",
-    "abstractKey": null
-  },
-  "kind": "Request",
-  "operation": {
-    "argumentDefinitions": [],
-    "kind": "Operation",
-    "name": "UserFragmentStoryQuery",
-    "selections": [
-      {
-        "alias": null,
-        "args": (v0/*: any*/),
-        "concreteType": null,
-        "kind": "LinkedField",
-        "name": "node",
-        "plural": false,
-        "selections": [
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "__typename",
-            "storageKey": null
-          },
-          {
-            "kind": "InlineFragment",
-            "selections": [
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "login",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "company",
-                "storageKey": null
-              },
-              {
-                "alias": null,
-                "args": null,
-                "kind": "ScalarField",
-                "name": "avatarUrl",
-                "storageKey": null
-              }
+        v1 = {
+            enumValues: null,
+            nullable: false,
+            plural: false,
+            type: 'String',
+        };
+    return {
+        fragment: {
+            argumentDefinitions: [],
+            kind: 'Fragment',
+            metadata: null,
+            name: 'UserFragmentStoryQuery',
+            selections: [
+                {
+                    alias: null,
+                    args: v0 /*: any*/,
+                    concreteType: null,
+                    kind: 'LinkedField',
+                    name: 'node',
+                    plural: false,
+                    selections: [
+                        {
+                            kind: 'InlineFragment',
+                            selections: [
+                                {
+                                    args: null,
+                                    kind: 'FragmentSpread',
+                                    name: 'UserFragment_user',
+                                },
+                            ],
+                            type: 'User',
+                            abstractKey: null,
+                        },
+                    ],
+                    storageKey: 'node(id:"test-id")',
+                },
             ],
-            "type": "User",
-            "abstractKey": null
-          },
-          {
-            "alias": null,
-            "args": null,
-            "kind": "ScalarField",
-            "name": "id",
-            "storageKey": null
-          }
-        ],
-        "storageKey": "node(id:\"test-id\")"
-      }
-    ]
-  },
-  "params": {
-    "cacheID": "c3eabae88cfd940696f13f0fc3bc8a59",
-    "id": null,
-    "metadata": {
-      "relayTestingSelectionTypeInfo": {
-        "node": {
-          "enumValues": null,
-          "nullable": true,
-          "plural": false,
-          "type": "Node"
+            type: 'Query',
+            abstractKey: null,
         },
-        "node.__typename": (v1/*: any*/),
-        "node.avatarUrl": {
-          "enumValues": null,
-          "nullable": false,
-          "plural": false,
-          "type": "URI"
+        kind: 'Request',
+        operation: {
+            argumentDefinitions: [],
+            kind: 'Operation',
+            name: 'UserFragmentStoryQuery',
+            selections: [
+                {
+                    alias: null,
+                    args: v0 /*: any*/,
+                    concreteType: null,
+                    kind: 'LinkedField',
+                    name: 'node',
+                    plural: false,
+                    selections: [
+                        {
+                            alias: null,
+                            args: null,
+                            kind: 'ScalarField',
+                            name: '__typename',
+                            storageKey: null,
+                        },
+                        {
+                            kind: 'InlineFragment',
+                            selections: [
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'login',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'company',
+                                    storageKey: null,
+                                },
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'avatarUrl',
+                                    storageKey: null,
+                                },
+                            ],
+                            type: 'User',
+                            abstractKey: null,
+                        },
+                        {
+                            alias: null,
+                            args: null,
+                            kind: 'ScalarField',
+                            name: 'id',
+                            storageKey: null,
+                        },
+                    ],
+                    storageKey: 'node(id:"test-id")',
+                },
+            ],
         },
-        "node.company": {
-          "enumValues": null,
-          "nullable": true,
-          "plural": false,
-          "type": "String"
+        params: {
+            cacheID: 'c3eabae88cfd940696f13f0fc3bc8a59',
+            id: null,
+            metadata: {
+                relayTestingSelectionTypeInfo: {
+                    node: {
+                        enumValues: null,
+                        nullable: true,
+                        plural: false,
+                        type: 'Node',
+                    },
+                    'node.__typename': v1 /*: any*/,
+                    'node.avatarUrl': {
+                        enumValues: null,
+                        nullable: false,
+                        plural: false,
+                        type: 'URI',
+                    },
+                    'node.company': {
+                        enumValues: null,
+                        nullable: true,
+                        plural: false,
+                        type: 'String',
+                    },
+                    'node.id': {
+                        enumValues: null,
+                        nullable: false,
+                        plural: false,
+                        type: 'ID',
+                    },
+                    'node.login': v1 /*: any*/,
+                },
+            },
+            name: 'UserFragmentStoryQuery',
+            operationKind: 'query',
+            text: 'query UserFragmentStoryQuery {\n  node(id: "test-id") {\n    __typename\n    ... on User {\n      ...UserFragment_user\n    }\n    id\n  }\n}\n\nfragment UserFragment_user on User {\n  login\n  company\n  avatarUrl\n}\n',
         },
-        "node.id": {
-          "enumValues": null,
-          "nullable": false,
-          "plural": false,
-          "type": "ID"
-        },
-        "node.login": (v1/*: any*/)
-      }
-    },
-    "name": "UserFragmentStoryQuery",
-    "operationKind": "query",
-    "text": "query UserFragmentStoryQuery {\n  node(id: \"test-id\") {\n    __typename\n    ... on User {\n      ...UserFragment_user\n    }\n    id\n  }\n}\n\nfragment UserFragment_user on User {\n  login\n  company\n  avatarUrl\n}\n"
-  }
-};
+    };
 })();
 
-(node as any).hash = "229258462933b4021003218dab26d320";
+(node as any).hash = '229258462933b4021003218dab26d320';
 
 export default node;
