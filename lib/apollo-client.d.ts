e91d24cffe4b3acd4e8c134f4f765b677df44b5c
lowsky <lowsky@users.noreply.github.com>
2025-08-26 10:04:03 +0200

    fix apollo typing related to masked fragments

---
+++ lib/apollo-client.d.ts
@@ -0,0 +1,8 @@
+// This import is necessary to ensure all Apollo Client imports
+// are still available to the rest of the application.
+import '@apollo/client';
+import type { GraphQLCodegenDataMasking } from '@apollo/client/masking';
+
+declare module '@apollo/client' {
+    interface TypeOverrides extends GraphQLCodegenDataMasking.TypeOverrides {}
+}
