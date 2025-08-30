f9b0e3db7e9d04fa60f675c10e0c64267468da52
lowsky <lowsky@users.noreply.github.com>
2025-08-30 12:00:23 +0200

    fix lint error in CommitWithStatuses

--- components/CommitWithStatuses/CommitWithStatuses.tsx
+++ components/CommitWithStatuses/CommitWithStatuses.tsx
@@ -18,38 +18,50 @@ interface CommitWithStatusesProps {
 const CommitWithStatuses: React.FC<CommitWithStatusesProps> = ({ commit }) => {
     const { author, commitUrl, authoredDate = '-?-', message = '-?-', status } = commit;
 
-    const mainMessage = message?.split('\n\n', 1);
+    const firstLineOfMessage = message?.split('\n\n', 1);
 
+    const authorUser = author?.user;
     return (
         <>
-            <PopoverRoot>
-                {
-                    // @ts-expect-error type error in snippet
-                    <PopoverTrigger>
-                        <span>
-                            <strong>{mainMessage}</strong> &nbsp;
-                            <Link className={styles.status} href={commitUrl} rel="noopener noreferrer nofollow">
-                                more
-                            </Link>
-                        </span>
-                    </PopoverTrigger>
-                }
-                {
-                    // @ts-expect-error snippet type error
-                    <PopoverContent>
-                        <PopoverBody>
-                            <PopoverArrow />
+            {authorUser && (
+                <PopoverRoot>
+                    {
+                        // @ts-expect-error type error in snippet
+                        <PopoverTrigger>
+                            <span>
+                                <strong>{firstLineOfMessage}</strong> &nbsp;
+                                <Link className={styles.status} href={commitUrl} rel="noopener noreferrer nofollow">
+                                    more
+                                </Link>
+                            </span>
+                        </PopoverTrigger>
+                    }
+                    {
+                        // @ts-expect-error snippet type error
+                        <PopoverContent>
                             <PopoverBody>
-                                <div className={styles.status}>
-                                    {<i>{new Date(authoredDate).toLocaleString()}</i>}
-                                    <CommitterInfo author={author?.user} />
-                                </div>
+                                <PopoverArrow />
+                                <PopoverBody>
+                                    <div className={styles.status}>
+                                        <i>{new Date(authoredDate).toLocaleString()}</i>
+                                        <CommitterInfo author={authorUser} />
+                                    </div>
+                                </PopoverBody>
                             </PopoverBody>
-                        </PopoverBody>
-                    </PopoverContent>
-                }
-            </PopoverRoot>
-
+                        </PopoverContent>
+                    }
+                </PopoverRoot>
+            )}
+            {!authorUser && (
+                <>
+                    <span>
+                        <strong>{firstLineOfMessage}</strong> &nbsp;
+                        <Link className={styles.status} href={commitUrl} rel="noopener noreferrer nofollow">
+                            more
+                        </Link>
+                    </span>
+                </>
+            )}
             {
                 // @ts-expect-error temporary ignore type mismatch
                 status && <CommitStatuses statuses={status.contexts} />
