278e2ccf1c26b1798e3fbc307b4c62ab7a4b5493
lowsky <lowsky@users.noreply.github.com>
2026-01-11 21:51:47 +0100

    cleanup, remove console.log, remove obsolete stuff, etc.

--- app/Page.story.tsx
+++ app/Page.story.tsx
@@ -4,6 +4,7 @@ import IndexPage from './page.mdx';
 
 const meta = {
     component: IndexPage,
+    tags: ['skipTesting', '!autodocs'],
 } satisfies Meta<typeof IndexPage>;
 export default meta;
 
@@ -14,40 +15,8 @@ export const Index: Story = {
         nextjs: {
             appDirectory: true,
             navigation: {
-                pathname: '/restful/lowsky/test-repo',
-                segments: [
-                    ['userName', 'lowsky'],
-                    ['repoName', 'test-repo'],
-                ],
-                query: {
-                    userName: 'lowsky',
-                    repoName: 'test-repo',
-                },
+                pathname: '/',
             },
         },
     },
 };
-
-/* this does currently not work at all, and need
-a different setup, with mocked Graphql Environment,
-and properly setup.
-Currently, failing because of:
-
- Invariant Violation: useRelayEnvironment: Expected to have found a Relay environment provided by a `RelayEnvironmentProvider` component. This usually means that useRelayEnvironment was used in a component that is not a descendant of a `RelayEnvironmentProvider`. Please make sure a `RelayEnvironmentProvider` has been rendered somewhere as a parent or ancestor of your component.
-
-  46 |     };
-  47 |
-> 48 |     const data = useLazyLoadQuery<relayPageQuery>(GithubQuery, variables);
-     |                  ^
-
-export const Relay = (props) => {
-    return <RelayRootMain {...props} />;
-};
-Relay.args = {
-    userName: "lowsky",
-    repoName: "dashboard"
-};
-Relay.parameters = {
-    chromatic: { disable: true },
-};
-*/
