1fca609a747245d27ecf66cb476917334c2462a2
lowsky <lowsky@users.noreply.github.com>
2026-03-29 23:54:30 +0200

    flatten the props of CommitWithStatuses

--- relay/__generated__/UserFragment_user.graphql.ts
+++ relay/__generated__/UserFragment_user.graphql.ts
@@ -9,50 +9,50 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-import { FragmentRefs } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
 export type UserFragment_user$data = {
-    readonly avatarUrl: any;
-    readonly company: string | null | undefined;
-    readonly login: string;
-    readonly ' $fragmentType': 'UserFragment_user';
+  readonly avatarUrl: any;
+  readonly company: string | null | undefined;
+  readonly login: string;
+  readonly " $fragmentType": "UserFragment_user";
 };
 export type UserFragment_user$key = {
-    readonly ' $data'?: UserFragment_user$data;
-    readonly ' $fragmentSpreads': FragmentRefs<'UserFragment_user'>;
+  readonly " $data"?: UserFragment_user$data;
+  readonly " $fragmentSpreads": FragmentRefs<"UserFragment_user">;
 };
 
 const node: ReaderFragment = {
-    argumentDefinitions: [],
-    kind: 'Fragment',
-    metadata: null,
-    name: 'UserFragment_user',
-    selections: [
-        {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'login',
-            storageKey: null,
-        },
-        {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'company',
-            storageKey: null,
-        },
-        {
-            alias: null,
-            args: null,
-            kind: 'ScalarField',
-            name: 'avatarUrl',
-            storageKey: null,
-        },
-    ],
-    type: 'User',
-    abstractKey: null,
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "UserFragment_user",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "login",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "company",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "avatarUrl",
+      "storageKey": null
+    }
+  ],
+  "type": "User",
+  "abstractKey": null
 };
 
-(node as any).hash = '9c7c07b31727412d37936f29be979876';
+(node as any).hash = "9c7c07b31727412d37936f29be979876";
 
 export default node;
