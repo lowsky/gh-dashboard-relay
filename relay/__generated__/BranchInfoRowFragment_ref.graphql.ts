17e73b1804453119a7e9ce47ecb506dd936627bc
lowsky <lowsky@users.noreply.github.com>
2026-05-07 22:36:21 +0200

    regeneraate relay files

--- relay/__generated__/BranchInfoRowFragment_ref.graphql.ts
+++ relay/__generated__/BranchInfoRowFragment_ref.graphql.ts
@@ -9,119 +9,130 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type BranchInfoRowFragment_ref$data = {
-  readonly associatedPullRequests: {
-    readonly edges: ReadonlyArray<{
-      readonly node: {
-        readonly id: string;
-        readonly " $fragmentSpreads": FragmentRefs<"PullRequestMergeFragment_ref">;
-      } | null | undefined;
-    } | null | undefined> | null | undefined;
-  };
-  readonly name: string;
-  readonly target: {
-    readonly " $fragmentSpreads": FragmentRefs<"CommitWithStatuses_commit">;
-  } | null | undefined;
-  readonly " $fragmentType": "BranchInfoRowFragment_ref";
+    readonly associatedPullRequests: {
+        readonly edges:
+            | ReadonlyArray<
+                  | {
+                        readonly node:
+                            | {
+                                  readonly id: string;
+                                  readonly ' $fragmentSpreads': FragmentRefs<'PullRequestMergeFragment_ref'>;
+                              }
+                            | null
+                            | undefined;
+                    }
+                  | null
+                  | undefined
+              >
+            | null
+            | undefined;
+    };
+    readonly name: string;
+    readonly target:
+        | {
+              readonly ' $fragmentSpreads': FragmentRefs<'CommitWithStatuses_commit'>;
+          }
+        | null
+        | undefined;
+    readonly ' $fragmentType': 'BranchInfoRowFragment_ref';
 };
 export type BranchInfoRowFragment_ref$key = {
-  readonly " $data"?: BranchInfoRowFragment_ref$data;
-  readonly " $fragmentSpreads": FragmentRefs<"BranchInfoRowFragment_ref">;
+    readonly ' $data'?: BranchInfoRowFragment_ref$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'BranchInfoRowFragment_ref'>;
 };
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "BranchInfoRowFragment_ref",
-  "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "name",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": null,
-      "kind": "LinkedField",
-      "name": "target",
-      "plural": false,
-      "selections": [
+    argumentDefinitions: [],
+    kind: 'Fragment',
+    metadata: null,
+    name: 'BranchInfoRowFragment_ref',
+    selections: [
         {
-          "args": null,
-          "kind": "FragmentSpread",
-          "name": "CommitWithStatuses_commit"
-        }
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": [
-        {
-          "kind": "Literal",
-          "name": "first",
-          "value": 1
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'name',
+            storageKey: null,
         },
         {
-          "kind": "Literal",
-          "name": "states",
-          "value": [
-            "OPEN"
-          ]
-        }
-      ],
-      "concreteType": "PullRequestConnection",
-      "kind": "LinkedField",
-      "name": "associatedPullRequests",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "PullRequestEdge",
-          "kind": "LinkedField",
-          "name": "edges",
-          "plural": true,
-          "selections": [
-            {
-              "alias": null,
-              "args": null,
-              "concreteType": "PullRequest",
-              "kind": "LinkedField",
-              "name": "node",
-              "plural": false,
-              "selections": [
+            alias: null,
+            args: null,
+            concreteType: null,
+            kind: 'LinkedField',
+            name: 'target',
+            plural: false,
+            selections: [
                 {
-                  "alias": null,
-                  "args": null,
-                  "kind": "ScalarField",
-                  "name": "id",
-                  "storageKey": null
+                    args: null,
+                    kind: 'FragmentSpread',
+                    name: 'CommitWithStatuses_commit',
+                },
+            ],
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: [
+                {
+                    kind: 'Literal',
+                    name: 'first',
+                    value: 1,
                 },
                 {
-                  "args": null,
-                  "kind": "FragmentSpread",
-                  "name": "PullRequestMergeFragment_ref"
-                }
-              ],
-              "storageKey": null
-            }
-          ],
-          "storageKey": null
-        }
-      ],
-      "storageKey": "associatedPullRequests(first:1,states:[\"OPEN\"])"
-    }
-  ],
-  "type": "Ref",
-  "abstractKey": null
+                    kind: 'Literal',
+                    name: 'states',
+                    value: ['OPEN'],
+                },
+            ],
+            concreteType: 'PullRequestConnection',
+            kind: 'LinkedField',
+            name: 'associatedPullRequests',
+            plural: false,
+            selections: [
+                {
+                    alias: null,
+                    args: null,
+                    concreteType: 'PullRequestEdge',
+                    kind: 'LinkedField',
+                    name: 'edges',
+                    plural: true,
+                    selections: [
+                        {
+                            alias: null,
+                            args: null,
+                            concreteType: 'PullRequest',
+                            kind: 'LinkedField',
+                            name: 'node',
+                            plural: false,
+                            selections: [
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'id',
+                                    storageKey: null,
+                                },
+                                {
+                                    args: null,
+                                    kind: 'FragmentSpread',
+                                    name: 'PullRequestMergeFragment_ref',
+                                },
+                            ],
+                            storageKey: null,
+                        },
+                    ],
+                    storageKey: null,
+                },
+            ],
+            storageKey: 'associatedPullRequests(first:1,states:["OPEN"])',
+        },
+    ],
+    type: 'Ref',
+    abstractKey: null,
 };
 
-(node as any).hash = "aaeea5298b83e9c444881c5772898bda";
+(node as any).hash = 'aaeea5298b83e9c444881c5772898bda';
 
 export default node;
--- relay/__generated__/CommitWithStatuses_commit.graphql.ts
+++ relay/__generated__/CommitWithStatuses_commit.graphql.ts
@@ -9,212 +9,225 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-export type StatusState = "ERROR" | "EXPECTED" | "FAILURE" | "PENDING" | "SUCCESS" | "%future added value";
-import { FragmentRefs } from "relay-runtime";
+export type StatusState = 'ERROR' | 'EXPECTED' | 'FAILURE' | 'PENDING' | 'SUCCESS' | '%future added value';
+import { FragmentRefs } from 'relay-runtime';
 export type CommitWithStatuses_commit$data = {
-  readonly author: {
-    readonly user: {
-      readonly avatarUrl: any;
-      readonly login: string;
-      readonly name: string | null | undefined;
-    } | null | undefined;
-  } | null | undefined;
-  readonly authoredDate: any;
-  readonly commitUrl: any;
-  readonly message: string;
-  readonly status: {
-    readonly commit: {
-      readonly oid: any;
-    } | null | undefined;
-    readonly contexts: ReadonlyArray<{
-      readonly avatarUrl: any | null | undefined;
-      readonly context: string;
-      readonly creator: {
-        readonly login: string;
-      } | null | undefined;
-      readonly description: string | null | undefined;
-      readonly state: StatusState;
-      readonly targetUrl: any | null | undefined;
-    }>;
-    readonly id: string;
-    readonly state: StatusState;
-  } | null | undefined;
-  readonly " $fragmentType": "CommitWithStatuses_commit";
+    readonly author:
+        | {
+              readonly user:
+                  | {
+                        readonly avatarUrl: any;
+                        readonly login: string;
+                        readonly name: string | null | undefined;
+                    }
+                  | null
+                  | undefined;
+          }
+        | null
+        | undefined;
+    readonly authoredDate: any;
+    readonly commitUrl: any;
+    readonly message: string;
+    readonly status:
+        | {
+              readonly commit:
+                  | {
+                        readonly oid: any;
+                    }
+                  | null
+                  | undefined;
+              readonly contexts: ReadonlyArray<{
+                  readonly avatarUrl: any | null | undefined;
+                  readonly context: string;
+                  readonly creator:
+                      | {
+                            readonly login: string;
+                        }
+                      | null
+                      | undefined;
+                  readonly description: string | null | undefined;
+                  readonly state: StatusState;
+                  readonly targetUrl: any | null | undefined;
+              }>;
+              readonly id: string;
+              readonly state: StatusState;
+          }
+        | null
+        | undefined;
+    readonly ' $fragmentType': 'CommitWithStatuses_commit';
 };
 export type CommitWithStatuses_commit$key = {
-  readonly " $data"?: CommitWithStatuses_commit$data;
-  readonly " $fragmentSpreads": FragmentRefs<"CommitWithStatuses_commit">;
+    readonly ' $data'?: CommitWithStatuses_commit$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'CommitWithStatuses_commit'>;
 };
 
-const node: ReaderFragment = (function(){
-var v0 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "avatarUrl",
-  "storageKey": null
-},
-v1 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "login",
-  "storageKey": null
-},
-v2 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "state",
-  "storageKey": null
-};
-return {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "CommitWithStatuses_commit",
-  "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "authoredDate",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "Status",
-      "kind": "LinkedField",
-      "name": "status",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "kind": "ScalarField",
-          "name": "id",
-          "storageKey": null
+const node: ReaderFragment = (function () {
+    var v0 = {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'avatarUrl',
+            storageKey: null,
         },
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "Commit",
-          "kind": "LinkedField",
-          "name": "commit",
-          "plural": false,
-          "selections": [
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "oid",
-              "storageKey": null
-            }
-          ],
-          "storageKey": null
+        v1 = {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'login',
+            storageKey: null,
         },
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "StatusContext",
-          "kind": "LinkedField",
-          "name": "contexts",
-          "plural": true,
-          "selections": [
-            (v0/*: any*/),
+        v2 = {
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'state',
+            storageKey: null,
+        };
+    return {
+        argumentDefinitions: [],
+        kind: 'Fragment',
+        metadata: null,
+        name: 'CommitWithStatuses_commit',
+        selections: [
             {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "context",
-              "storageKey": null
+                alias: null,
+                args: null,
+                kind: 'ScalarField',
+                name: 'authoredDate',
+                storageKey: null,
             },
             {
-              "alias": null,
-              "args": null,
-              "concreteType": null,
-              "kind": "LinkedField",
-              "name": "creator",
-              "plural": false,
-              "selections": [
-                (v1/*: any*/)
-              ],
-              "storageKey": null
+                alias: null,
+                args: null,
+                concreteType: 'Status',
+                kind: 'LinkedField',
+                name: 'status',
+                plural: false,
+                selections: [
+                    {
+                        alias: null,
+                        args: null,
+                        kind: 'ScalarField',
+                        name: 'id',
+                        storageKey: null,
+                    },
+                    {
+                        alias: null,
+                        args: null,
+                        concreteType: 'Commit',
+                        kind: 'LinkedField',
+                        name: 'commit',
+                        plural: false,
+                        selections: [
+                            {
+                                alias: null,
+                                args: null,
+                                kind: 'ScalarField',
+                                name: 'oid',
+                                storageKey: null,
+                            },
+                        ],
+                        storageKey: null,
+                    },
+                    {
+                        alias: null,
+                        args: null,
+                        concreteType: 'StatusContext',
+                        kind: 'LinkedField',
+                        name: 'contexts',
+                        plural: true,
+                        selections: [
+                            v0 /*: any*/,
+                            {
+                                alias: null,
+                                args: null,
+                                kind: 'ScalarField',
+                                name: 'context',
+                                storageKey: null,
+                            },
+                            {
+                                alias: null,
+                                args: null,
+                                concreteType: null,
+                                kind: 'LinkedField',
+                                name: 'creator',
+                                plural: false,
+                                selections: [v1 /*: any*/],
+                                storageKey: null,
+                            },
+                            v2 /*: any*/,
+                            {
+                                alias: null,
+                                args: null,
+                                kind: 'ScalarField',
+                                name: 'description',
+                                storageKey: null,
+                            },
+                            {
+                                alias: null,
+                                args: null,
+                                kind: 'ScalarField',
+                                name: 'targetUrl',
+                                storageKey: null,
+                            },
+                        ],
+                        storageKey: null,
+                    },
+                    v2 /*: any*/,
+                ],
+                storageKey: null,
             },
-            (v2/*: any*/),
             {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "description",
-              "storageKey": null
+                alias: null,
+                args: null,
+                kind: 'ScalarField',
+                name: 'commitUrl',
+                storageKey: null,
             },
             {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "targetUrl",
-              "storageKey": null
-            }
-          ],
-          "storageKey": null
-        },
-        (v2/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "commitUrl",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "message",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "GitActor",
-      "kind": "LinkedField",
-      "name": "author",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "User",
-          "kind": "LinkedField",
-          "name": "user",
-          "plural": false,
-          "selections": [
-            (v1/*: any*/),
-            {
-              "alias": null,
-              "args": null,
-              "kind": "ScalarField",
-              "name": "name",
-              "storageKey": null
+                alias: null,
+                args: null,
+                kind: 'ScalarField',
+                name: 'message',
+                storageKey: null,
             },
-            (v0/*: any*/)
-          ],
-          "storageKey": null
-        }
-      ],
-      "storageKey": null
-    }
-  ],
-  "type": "Commit",
-  "abstractKey": null
-};
+            {
+                alias: null,
+                args: null,
+                concreteType: 'GitActor',
+                kind: 'LinkedField',
+                name: 'author',
+                plural: false,
+                selections: [
+                    {
+                        alias: null,
+                        args: null,
+                        concreteType: 'User',
+                        kind: 'LinkedField',
+                        name: 'user',
+                        plural: false,
+                        selections: [
+                            v1 /*: any*/,
+                            {
+                                alias: null,
+                                args: null,
+                                kind: 'ScalarField',
+                                name: 'name',
+                                storageKey: null,
+                            },
+                            v0 /*: any*/,
+                        ],
+                        storageKey: null,
+                    },
+                ],
+                storageKey: null,
+            },
+        ],
+        type: 'Commit',
+        abstractKey: null,
+    };
 })();
 
-(node as any).hash = "87150de4c1c7defd35908a82afe812a3";
+(node as any).hash = '87150de4c1c7defd35908a82afe812a3';
 
 export default node;
