9a8fed68ef998c7020393bb57b91df0377bb396a
lowsky <lowsky@users.noreply.github.com>
2026-05-28 20:54:12 +0200

    upgrade generated glue code for relay

--- app/relay/[userName]/__generated__/UserWithReposFragment_user.graphql.ts
+++ app/relay/[userName]/__generated__/UserWithReposFragment_user.graphql.ts
@@ -8,64 +8,64 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type UserWithReposFragment_user$data = {
-  readonly repoList: {
-    readonly " $fragmentSpreads": FragmentRefs<"RepoListFragment_user">;
-  };
-  readonly user: {
-    readonly " $fragmentSpreads": FragmentRefs<"UserFragment_user">;
-  };
-  readonly " $fragmentType": "UserWithReposFragment_user";
+    readonly repoList: {
+        readonly ' $fragmentSpreads': FragmentRefs<'RepoListFragment_user'>;
+    };
+    readonly user: {
+        readonly ' $fragmentSpreads': FragmentRefs<'UserFragment_user'>;
+    };
+    readonly ' $fragmentType': 'UserWithReposFragment_user';
 };
 export type UserWithReposFragment_user$key = {
-  readonly " $data"?: UserWithReposFragment_user$data;
-  readonly " $fragmentSpreads": FragmentRefs<"UserWithReposFragment_user">;
+    readonly ' $data'?: UserWithReposFragment_user$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'UserWithReposFragment_user'>;
 };
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "UserWithReposFragment_user",
-  "selections": [
-    {
-      "fragment": {
-        "kind": "InlineFragment",
-        "selections": [
-          {
-            "args": null,
-            "kind": "FragmentSpread",
-            "name": "UserFragment_user"
-          }
-        ],
-        "type": "User",
-        "abstractKey": null
-      },
-      "kind": "AliasedInlineFragmentSpread",
-      "name": "user"
-    },
-    {
-      "fragment": {
-        "kind": "InlineFragment",
-        "selections": [
-          {
-            "args": null,
-            "kind": "FragmentSpread",
-            "name": "RepoListFragment_user"
-          }
-        ],
-        "type": "User",
-        "abstractKey": null
-      },
-      "kind": "AliasedInlineFragmentSpread",
-      "name": "repoList"
-    }
-  ],
-  "type": "User",
-  "abstractKey": null
+    argumentDefinitions: [],
+    kind: 'Fragment',
+    metadata: null,
+    name: 'UserWithReposFragment_user',
+    selections: [
+        {
+            fragment: {
+                kind: 'InlineFragment',
+                selections: [
+                    {
+                        args: null,
+                        kind: 'FragmentSpread',
+                        name: 'UserFragment_user',
+                    },
+                ],
+                type: 'User',
+                abstractKey: null,
+            },
+            kind: 'AliasedInlineFragmentSpread',
+            name: 'user',
+        },
+        {
+            fragment: {
+                kind: 'InlineFragment',
+                selections: [
+                    {
+                        args: null,
+                        kind: 'FragmentSpread',
+                        name: 'RepoListFragment_user',
+                    },
+                ],
+                type: 'User',
+                abstractKey: null,
+            },
+            kind: 'AliasedInlineFragmentSpread',
+            name: 'repoList',
+        },
+    ],
+    type: 'User',
+    abstractKey: null,
 };
 
-(node as any).hash = "a3b99b1edb2356fbdc970fa881cb65a8";
+(node as any).hash = 'a3b99b1edb2356fbdc970fa881cb65a8';
 
 export default node;
--- relay/__generated__/BranchInfoRowFragment_ref.graphql.ts
+++ relay/__generated__/BranchInfoRowFragment_ref.graphql.ts
@@ -8,145 +8,159 @@
 // @ts-nocheck
 
 import { ReaderFragment } from 'relay-runtime';
-import { FragmentRefs } from "relay-runtime";
+import { FragmentRefs } from 'relay-runtime';
 export type BranchInfoRowFragment_ref$data = {
-  readonly associatedPullRequests: {
-    readonly edges: ReadonlyArray<{
-      readonly node: {
-        readonly id: string;
-        readonly pr: {
-          readonly " $fragmentSpreads": FragmentRefs<"PullRequestMergeFragment_ref">;
-        };
-      } | null | undefined;
-    } | null | undefined> | null | undefined;
-  };
-  readonly name: string;
-  readonly target: {
-    readonly commit: {
-      readonly " $fragmentSpreads": FragmentRefs<"CommitWithStatuses_commit">;
-    } | null | undefined;
-  } | null | undefined;
-  readonly " $fragmentType": "BranchInfoRowFragment_ref";
+    readonly associatedPullRequests: {
+        readonly edges:
+            | ReadonlyArray<
+                  | {
+                        readonly node:
+                            | {
+                                  readonly id: string;
+                                  readonly pr: {
+                                      readonly ' $fragmentSpreads': FragmentRefs<'PullRequestMergeFragment_ref'>;
+                                  };
+                              }
+                            | null
+                            | undefined;
+                    }
+                  | null
+                  | undefined
+              >
+            | null
+            | undefined;
+    };
+    readonly name: string;
+    readonly target:
+        | {
+              readonly commit:
+                  | {
+                        readonly ' $fragmentSpreads': FragmentRefs<'CommitWithStatuses_commit'>;
+                    }
+                  | null
+                  | undefined;
+          }
+        | null
+        | undefined;
+    readonly ' $fragmentType': 'BranchInfoRowFragment_ref';
 };
 export type BranchInfoRowFragment_ref$key = {
-  readonly " $data"?: BranchInfoRowFragment_ref$data;
-  readonly " $fragmentSpreads": FragmentRefs<"BranchInfoRowFragment_ref">;
+    readonly ' $data'?: BranchInfoRowFragment_ref$data;
+    readonly ' $fragmentSpreads': FragmentRefs<'BranchInfoRowFragment_ref'>;
 };
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
-  "kind": "Fragment",
-  "metadata": null,
-  "name": "BranchInfoRowFragment_ref",
-  "selections": [
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "name",
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": null,
-      "kind": "LinkedField",
-      "name": "target",
-      "plural": false,
-      "selections": [
+    argumentDefinitions: [],
+    kind: 'Fragment',
+    metadata: null,
+    name: 'BranchInfoRowFragment_ref',
+    selections: [
         {
-          "fragment": {
-            "kind": "InlineFragment",
-            "selections": [
-              {
-                "args": null,
-                "kind": "FragmentSpread",
-                "name": "CommitWithStatuses_commit"
-              }
-            ],
-            "type": "Commit",
-            "abstractKey": null
-          },
-          "kind": "AliasedInlineFragmentSpread",
-          "name": "commit"
-        }
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": [
-        {
-          "kind": "Literal",
-          "name": "first",
-          "value": 1
+            alias: null,
+            args: null,
+            kind: 'ScalarField',
+            name: 'name',
+            storageKey: null,
         },
         {
-          "kind": "Literal",
-          "name": "states",
-          "value": [
-            "OPEN"
-          ]
-        }
-      ],
-      "concreteType": "PullRequestConnection",
-      "kind": "LinkedField",
-      "name": "associatedPullRequests",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "PullRequestEdge",
-          "kind": "LinkedField",
-          "name": "edges",
-          "plural": true,
-          "selections": [
-            {
-              "alias": null,
-              "args": null,
-              "concreteType": "PullRequest",
-              "kind": "LinkedField",
-              "name": "node",
-              "plural": false,
-              "selections": [
+            alias: null,
+            args: null,
+            concreteType: null,
+            kind: 'LinkedField',
+            name: 'target',
+            plural: false,
+            selections: [
                 {
-                  "alias": null,
-                  "args": null,
-                  "kind": "ScalarField",
-                  "name": "id",
-                  "storageKey": null
+                    fragment: {
+                        kind: 'InlineFragment',
+                        selections: [
+                            {
+                                args: null,
+                                kind: 'FragmentSpread',
+                                name: 'CommitWithStatuses_commit',
+                            },
+                        ],
+                        type: 'Commit',
+                        abstractKey: null,
+                    },
+                    kind: 'AliasedInlineFragmentSpread',
+                    name: 'commit',
+                },
+            ],
+            storageKey: null,
+        },
+        {
+            alias: null,
+            args: [
+                {
+                    kind: 'Literal',
+                    name: 'first',
+                    value: 1,
                 },
                 {
-                  "fragment": {
-                    "kind": "InlineFragment",
-                    "selections": [
-                      {
-                        "args": null,
-                        "kind": "FragmentSpread",
-                        "name": "PullRequestMergeFragment_ref"
-                      }
+                    kind: 'Literal',
+                    name: 'states',
+                    value: ['OPEN'],
+                },
+            ],
+            concreteType: 'PullRequestConnection',
+            kind: 'LinkedField',
+            name: 'associatedPullRequests',
+            plural: false,
+            selections: [
+                {
+                    alias: null,
+                    args: null,
+                    concreteType: 'PullRequestEdge',
+                    kind: 'LinkedField',
+                    name: 'edges',
+                    plural: true,
+                    selections: [
+                        {
+                            alias: null,
+                            args: null,
+                            concreteType: 'PullRequest',
+                            kind: 'LinkedField',
+                            name: 'node',
+                            plural: false,
+                            selections: [
+                                {
+                                    alias: null,
+                                    args: null,
+                                    kind: 'ScalarField',
+                                    name: 'id',
+                                    storageKey: null,
+                                },
+                                {
+                                    fragment: {
+                                        kind: 'InlineFragment',
+                                        selections: [
+                                            {
+                                                args: null,
+                                                kind: 'FragmentSpread',
+                                                name: 'PullRequestMergeFragment_ref',
+                                            },
+                                        ],
+                                        type: 'PullRequest',
+                                        abstractKey: null,
+                                    },
+                                    kind: 'AliasedInlineFragmentSpread',
+                                    name: 'pr',
+                                },
+                            ],
+                            storageKey: null,
+                        },
                     ],
-                    "type": "PullRequest",
-                    "abstractKey": null
-                  },
-                  "kind": "AliasedInlineFragmentSpread",
-                  "name": "pr"
-                }
-              ],
-              "storageKey": null
-            }
-          ],
-          "storageKey": null
-        }
-      ],
-      "storageKey": "associatedPullRequests(first:1,states:[\"OPEN\"])"
-    }
-  ],
-  "type": "Ref",
-  "abstractKey": null
+                    storageKey: null,
+                },
+            ],
+            storageKey: 'associatedPullRequests(first:1,states:["OPEN"])',
+        },
+    ],
+    type: 'Ref',
+    abstractKey: null,
 };
 
-(node as any).hash = "97ab3924798d504ee5944eb2e6cf2ff1";
+(node as any).hash = '97ab3924798d504ee5944eb2e6cf2ff1';
 
 export default node;
